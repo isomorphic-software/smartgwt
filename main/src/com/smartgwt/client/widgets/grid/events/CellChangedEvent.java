c47aca84a2490b2b54e0aa4c13c6dea4818a6103
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-01-02 03:51:06 +0000

    initial commit

---
+++ main/src/com/smartgwt/client/widgets/grid/events/CellChangedEvent.java
@@ -0,0 +1,186 @@
+/*
+ * SmartGWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * SmartGWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  SmartGWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+ 
+package com.smartgwt.client.widgets.grid.events;
+
+
+
+import com.smartgwt.client.event.*;
+import com.smartgwt.client.core.*;
+import com.smartgwt.client.types.*;
+import com.smartgwt.client.data.*;
+import com.smartgwt.client.data.events.*;
+import com.smartgwt.client.rpc.*;
+import com.smartgwt.client.widgets.*;
+import com.smartgwt.client.widgets.events.*;
+import com.smartgwt.client.widgets.form.*;
+import com.smartgwt.client.widgets.form.validator.*;
+import com.smartgwt.client.widgets.form.fields.*;
+import com.smartgwt.client.widgets.tile.*;
+import com.smartgwt.client.widgets.tile.events.*;
+import com.smartgwt.client.widgets.grid.*;
+import com.smartgwt.client.widgets.grid.events.*;
+import com.smartgwt.client.widgets.layout.*;
+import com.smartgwt.client.widgets.menu.*;
+import com.smartgwt.client.widgets.tab.*;
+import com.smartgwt.client.widgets.toolbar.*;
+import com.smartgwt.client.widgets.tree.*;
+import com.smartgwt.client.widgets.tree.events.*;
+import com.smartgwt.client.widgets.viewer.*;
+import com.smartgwt.client.widgets.calendar.*;
+import com.smartgwt.client.widgets.calendar.events.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
+import com.smartgwt.client.util.JSOHelper;
+import com.smartgwt.client.util.EnumUtil;
+import com.google.gwt.event.shared.*;
+import com.google.gwt.event.logical.shared.HasHandlers;
+public class CellChangedEvent extends BrowserEvent<CellChangedHandler>  {
+
+  /**
+   * Handler type.
+   */
+  private static Type<CellChangedHandler> TYPE;
+
+  /**
+   * Fires a open event on all registered handlers in the handler manager.If no
+   * such handlers exist, this method will do nothing.
+   *
+   * @param <S> The event source
+   * @param source the source of the handlers
+   * @param target the target
+   */
+  public static <S extends HasCellChangedHandlers & HasHandlers> void fire(
+      S source, JavaScriptObject jsObj) {
+    if (TYPE != null) {
+      HandlerManager handlers = source.getHandlers();
+      if (handlers != null) {
+        CellChangedEvent event = new CellChangedEvent(jsObj);
+        handlers.fireEvent(event);
+      }
+    }
+  }
+
+  /**
+   * Gets the type associated with this event.
+   *
+   * @return returns the handler type
+   */
+  public static Type<CellChangedHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<CellChangedHandler>();
+    }
+    return TYPE;
+  }
+
+
+  @Override
+  protected void dispatch(CellChangedHandler handler) {
+    handler.onCellChanged(this);
+  }
+
+  // Because of type erasure, our static type is
+  // wild carded, yet the "real" type should use our I param.
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected final Type<CellChangedHandler> getAssociatedType() {
+    return TYPE;
+  }
+
+    public CellChangedEvent(JavaScriptObject jsObj) {
+        super(jsObj);
+    }
+
+
+
+    /**
+     * record for the cell being changed
+     *
+     * @return record for the cell being changed
+     */
+    public  native ListGridRecord getRecord() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+            var retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            if(retVal == null) {
+                retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            }
+            return retVal;
+    }-*/;
+
+    /**
+     * new value for the cell
+     *
+     * @return new value for the cell
+     */
+    public  native Object getNewValue() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return $wnd.SmartGWT.convertToJavaType(jsObj.newValue);
+    }-*/;
+
+    /**
+     * old value for the cell
+     *
+     * @return old value for the cell
+     */
+    public  native Object getOldValue() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return $wnd.SmartGWT.convertToJavaType(jsObj.oldValue);
+    }-*/;
+
+    /**
+     * row number for the cell
+     *
+     * @return row number for the cell
+     */
+    public  native int getRowNum() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.rowNum;
+    }-*/;
+
+    /**
+     * column number of the cell
+     *
+     * @return column number of the cell
+     */
+    public  native int getColNum() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.colNum;
+    }-*/;
+
+    /**
+     * grid where cell was changed.  Also available as "this"
+     *
+     * @return grid where cell was changed.  Also available as "this"
+     */
+    public  native ListGrid getGrid() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        var retVal = @com.smartgwt.client.widgets.grid.ListGrid::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.grid);
+        if(retVal == null) {
+            retVal = @com.smartgwt.client.widgets.grid.ListGrid::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.grid);
+        }
+        return retVal;
+    }-*/;
+
+
+}
---
+++ main/src/com/smartgwt/client/widgets/grid/events/EditorExitEvent.java
@@ -0,0 +1,200 @@
+/*
+ * SmartGWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * SmartGWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  SmartGWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+ 
+package com.smartgwt.client.widgets.grid.events;
+
+
+
+import com.smartgwt.client.event.*;
+import com.smartgwt.client.core.*;
+import com.smartgwt.client.types.*;
+import com.smartgwt.client.data.*;
+import com.smartgwt.client.data.events.*;
+import com.smartgwt.client.rpc.*;
+import com.smartgwt.client.widgets.*;
+import com.smartgwt.client.widgets.events.*;
+import com.smartgwt.client.widgets.form.*;
+import com.smartgwt.client.widgets.form.validator.*;
+import com.smartgwt.client.widgets.form.fields.*;
+import com.smartgwt.client.widgets.tile.*;
+import com.smartgwt.client.widgets.tile.events.*;
+import com.smartgwt.client.widgets.grid.*;
+import com.smartgwt.client.widgets.grid.events.*;
+import com.smartgwt.client.widgets.layout.*;
+import com.smartgwt.client.widgets.menu.*;
+import com.smartgwt.client.widgets.tab.*;
+import com.smartgwt.client.widgets.toolbar.*;
+import com.smartgwt.client.widgets.tree.*;
+import com.smartgwt.client.widgets.tree.events.*;
+import com.smartgwt.client.widgets.viewer.*;
+import com.smartgwt.client.widgets.calendar.*;
+import com.smartgwt.client.widgets.calendar.events.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
+import com.smartgwt.client.util.JSOHelper;
+import com.smartgwt.client.util.EnumUtil;
+import com.google.gwt.event.shared.*;
+import com.google.gwt.event.logical.shared.HasHandlers;
+public class EditorExitEvent extends AbstractSmartEvent<EditorExitHandler>  implements Cancellable {
+    private boolean cancel = false;
+
+  /**
+   * Handler type.
+   */
+  private static Type<EditorExitHandler> TYPE;
+
+  /**
+   * Fires a open event on all registered handlers in the handler manager.If no
+   * such handlers exist, this method will do nothing.
+   *
+   * @param <S> The event source
+   * @param source the source of the handlers
+   * @param target the target
+   */
+  public static <S extends HasEditorExitHandlers & HasHandlers> void fire(
+      S source, JavaScriptObject jsObj) {
+    if (TYPE != null) {
+      HandlerManager handlers = source.getHandlers();
+      if (handlers != null) {
+        EditorExitEvent event = new EditorExitEvent(jsObj);
+        handlers.fireEvent(event);
+      }
+    }
+  }
+
+  /**
+   * Gets the type associated with this event.
+   *
+   * @return returns the handler type
+   */
+  public static Type<EditorExitHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<EditorExitHandler>();
+    }
+    return TYPE;
+  }
+
+
+  @Override
+  protected void dispatch(EditorExitHandler handler) {
+    handler.onEditorExit(this);
+  }
+
+  // Because of type erasure, our static type is
+  // wild carded, yet the "real" type should use our I param.
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected final Type<EditorExitHandler> getAssociatedType() {
+    return TYPE;
+  }
+
+    public EditorExitEvent(JavaScriptObject jsObj) {
+        super(jsObj);
+    }
+
+
+    /**
+     * Returning false from this method will cancel the default behavior                      (for example saving the row) and leave the editor visible and focus                      in this edit cell.
+     */
+    public void cancel() {
+        cancel = true;
+    }
+
+    /**
+     * @return true if cancelled
+     */
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    /**
+     * What interaction triggered this                                                          edit cell exit
+     *
+     * @return What interaction triggered this                                                          edit cell exit
+     */
+    public  native EditCompletionEvent getEditCompletionEvent() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return @com.smartgwt.client.types.EditCompletionEvent::valueOf(Ljava/lang/String;)(jsObj.editCompletionEvent);
+    }-*/;
+
+    /**
+     * record for the cell being edited
+     *
+     * @return record for the cell being edited
+     */
+    public  native ListGridRecord getRecord() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+            var retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            if(retVal == null) {
+                retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            }
+            return retVal;
+    }-*/;
+
+    /**
+     * new value for the cell being edited
+     *
+     * @return new value for the cell being edited
+     */
+    public  native Object getNewValue() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return $wnd.SmartGWT.convertToJavaType(jsObj.newValue);
+    }-*/;
+
+    /**
+     * row number for the cell
+     *
+     * @return row number for the cell
+     */
+    public  native int getRowNum() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.rowNum;
+    }-*/;
+
+    /**
+     * column number of the cell
+     *
+     * @return column number of the cell
+     */
+    public  native int getColNum() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.colNum;
+    }-*/;
+
+    /**
+     * ListGrid to which this field belongs
+     *
+     * @return ListGrid to which this field belongs
+     */
+    public  native ListGrid getGrid() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        var retVal = @com.smartgwt.client.widgets.grid.ListGrid::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.grid);
+        if(retVal == null) {
+            retVal = @com.smartgwt.client.widgets.grid.ListGrid::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.grid);
+        }
+        return retVal;
+    }-*/;
+
+
+}
