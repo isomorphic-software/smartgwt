0ca00a10ed32af9d35675e8ceb84a89b1084e851
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-01-02 03:52:41 +0000

    initial commit

---
+++ trunk/main/src/com/smartgwt/client/widgets/grid/events/RowEditorExitEvent.java
@@ -0,0 +1,180 @@
+/*
+ * SmartGWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * SmartGWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  SmartGWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+ 
+package com.smartgwt.client.widgets.grid.events;
+
+
+
+import com.smartgwt.client.event.*;
+import com.smartgwt.client.core.*;
+import com.smartgwt.client.types.*;
+import com.smartgwt.client.data.*;
+import com.smartgwt.client.data.events.*;
+import com.smartgwt.client.rpc.*;
+import com.smartgwt.client.widgets.*;
+import com.smartgwt.client.widgets.events.*;
+import com.smartgwt.client.widgets.form.*;
+import com.smartgwt.client.widgets.form.validator.*;
+import com.smartgwt.client.widgets.form.fields.*;
+import com.smartgwt.client.widgets.tile.*;
+import com.smartgwt.client.widgets.tile.events.*;
+import com.smartgwt.client.widgets.grid.*;
+import com.smartgwt.client.widgets.grid.events.*;
+import com.smartgwt.client.widgets.layout.*;
+import com.smartgwt.client.widgets.menu.*;
+import com.smartgwt.client.widgets.tab.*;
+import com.smartgwt.client.widgets.toolbar.*;
+import com.smartgwt.client.widgets.tree.*;
+import com.smartgwt.client.widgets.tree.events.*;
+import com.smartgwt.client.widgets.viewer.*;
+import com.smartgwt.client.widgets.calendar.*;
+import com.smartgwt.client.widgets.calendar.events.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
+import com.smartgwt.client.util.JSOHelper;
+import com.smartgwt.client.util.EnumUtil;
+import com.google.gwt.event.shared.*;
+import com.google.gwt.event.logical.shared.HasHandlers;
+public class RowEditorExitEvent extends BrowserEvent<RowEditorExitHandler>  implements Cancellable {
+    private boolean cancel = false;
+
+  /**
+   * Handler type.
+   */
+  private static Type<RowEditorExitHandler> TYPE;
+
+  /**
+   * Fires a open event on all registered handlers in the handler manager.If no
+   * such handlers exist, this method will do nothing.
+   *
+   * @param <S> The event source
+   * @param source the source of the handlers
+   * @param target the target
+   */
+  public static <S extends HasRowEditorExitHandlers & HasHandlers> void fire(
+      S source, JavaScriptObject jsObj) {
+    if (TYPE != null) {
+      HandlerManager handlers = source.getHandlers();
+      if (handlers != null) {
+        RowEditorExitEvent event = new RowEditorExitEvent(jsObj);
+        handlers.fireEvent(event);
+      }
+    }
+  }
+
+  /**
+   * Gets the type associated with this event.
+   *
+   * @return returns the handler type
+   */
+  public static Type<RowEditorExitHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<RowEditorExitHandler>();
+    }
+    return TYPE;
+  }
+
+
+  @Override
+  protected void dispatch(RowEditorExitHandler handler) {
+    handler.onRowEditorExit(this);
+  }
+
+  // Because of type erasure, our static type is
+  // wild carded, yet the "real" type should use our I param.
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected final Type<RowEditorExitHandler> getAssociatedType() {
+    return TYPE;
+  }
+
+    public RowEditorExitEvent(JavaScriptObject jsObj) {
+        super(jsObj);
+    }
+
+
+    /**
+     * Returning false from this method will cancel the default behavior                      (for example saving the row) and leave the editor visible and focus                      in this edit cell.
+     */
+    public void cancel() {
+        cancel = true;
+    }
+
+    /**
+     * @return true if cancelled
+     */
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    /**
+     * How was the edit completion fired?
+     *
+     * @return How was the edit completion fired?
+     */
+    public  native EditCompletionEvent getEditCompletionEvent() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return @com.smartgwt.client.types.EditCompletionEvent::valueOf(Ljava/lang/String;)(jsObj.editCompletionEvent);
+    }-*/;
+
+    /**
+     * record for the cell being edited
+     *
+     * @return record for the cell being edited
+     */
+    public  native ListGridRecord getRecord() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+            var retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            if(retVal == null) {
+                retVal = @com.smartgwt.client.widgets.grid.ListGridRecord::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.record);
+            }
+            return retVal;
+    }-*/;
+
+    /**
+     * row number for the row being left
+     *
+     * @return row number for the row being left
+     */
+    public  native int getRowNum() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.rowNum;
+    }-*/;
+
+
+
+
+    /**
+     * edit values for the current row
+     *
+     * @return edit values for the current row
+     */
+    //TODO
+    public native JavaScriptObject getEditValues() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        return jsObj.editValues;
+    }-*/;
+
+
+}
