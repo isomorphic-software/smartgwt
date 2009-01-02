ed71c5a1e3971693c9ef6ba47c5c0a17854c7b10
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-01-02 03:18:14 +0000

    initial commit

---
+++ trunk/main/src/com/smartgwt/client/widgets/form/fields/events/BlurEvent.java
@@ -0,0 +1,146 @@
+/*
+ * SmartGWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * SmartGWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  SmartGWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+ 
+package com.smartgwt.client.widgets.form.fields.events;
+
+
+
+import com.smartgwt.client.event.*;
+import com.smartgwt.client.core.*;
+import com.smartgwt.client.types.*;
+import com.smartgwt.client.data.*;
+import com.smartgwt.client.data.events.*;
+import com.smartgwt.client.rpc.*;
+import com.smartgwt.client.widgets.*;
+import com.smartgwt.client.widgets.events.*;
+import com.smartgwt.client.widgets.form.*;
+import com.smartgwt.client.widgets.form.validator.*;
+import com.smartgwt.client.widgets.form.fields.*;
+import com.smartgwt.client.widgets.tile.*;
+import com.smartgwt.client.widgets.tile.events.*;
+import com.smartgwt.client.widgets.grid.*;
+import com.smartgwt.client.widgets.grid.events.*;
+import com.smartgwt.client.widgets.layout.*;
+import com.smartgwt.client.widgets.menu.*;
+import com.smartgwt.client.widgets.tab.*;
+import com.smartgwt.client.widgets.toolbar.*;
+import com.smartgwt.client.widgets.tree.*;
+import com.smartgwt.client.widgets.tree.events.*;
+import com.smartgwt.client.widgets.viewer.*;
+import com.smartgwt.client.widgets.calendar.*;
+import com.smartgwt.client.widgets.calendar.events.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
+import com.smartgwt.client.util.JSOHelper;
+import com.smartgwt.client.util.EnumUtil;
+import com.google.gwt.event.shared.*;
+import com.google.gwt.event.logical.shared.HasHandlers;
+public class BlurEvent extends AbstractSmartEvent<BlurHandler>  {
+
+  /**
+   * Handler type.
+   */
+  private static Type<BlurHandler> TYPE;
+
+  /**
+   * Fires a open event on all registered handlers in the handler manager.If no
+   * such handlers exist, this method will do nothing.
+   *
+   * @param <S> The event source
+   * @param source the source of the handlers
+   * @param target the target
+   */
+  public static <S extends HasBlurHandlers & HasHandlers> void fire(
+      S source, JavaScriptObject jsObj) {
+    if (TYPE != null) {
+      HandlerManager handlers = source.getHandlers();
+      if (handlers != null) {
+        BlurEvent event = new BlurEvent(jsObj);
+        handlers.fireEvent(event);
+      }
+    }
+  }
+
+  /**
+   * Gets the type associated with this event.
+   *
+   * @return returns the handler type
+   */
+  public static Type<BlurHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<BlurHandler>();
+    }
+    return TYPE;
+  }
+
+
+  @Override
+  protected void dispatch(BlurHandler handler) {
+    handler.onBlur(this);
+  }
+
+  // Because of type erasure, our static type is
+  // wild carded, yet the "real" type should use our I param.
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected final Type<BlurHandler> getAssociatedType() {
+    return TYPE;
+  }
+
+    public BlurEvent(JavaScriptObject jsObj) {
+        super(jsObj);
+    }
+
+
+
+    /**
+     * the managing DynamicForm instance
+     *
+     * @return the managing DynamicForm instance
+     */
+    public  native DynamicForm getForm() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+        var retVal = @com.smartgwt.client.widgets.form.DynamicForm::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.form);
+        if(retVal == null) {
+            retVal = @com.smartgwt.client.widgets.form.DynamicForm::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.form);
+        }
+        return retVal;
+    }-*/;
+
+    /**
+     * the form item itself (also available as "this")
+     *
+     * @return the form item itself (also available as "this")
+     */
+    public  native FormItem getItem() /*-{
+        var jsObj = this.@com.smartgwt.client.event.AbstractSmartEvent::jsObj;
+            var retVal = @com.smartgwt.client.widgets.form.fields.FormItem::getOrCreateRef(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.item);
+            if(retVal == null) {
+                retVal = @com.smartgwt.client.widgets.form.fields.FormItem::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj.item);
+            }
+            return retVal;
+    }-*/;
+
+
+}
