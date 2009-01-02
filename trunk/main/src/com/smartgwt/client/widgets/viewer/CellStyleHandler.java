aa7586a20d036ee593c30c9718933b96bbafe22e
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-01-02 04:05:34 +0000

    initial commit

---
+++ trunk/main/src/com/smartgwt/client/widgets/viewer/CellStyleHandler.java
@@ -0,0 +1,6 @@
+package com.smartgwt.client.widgets.viewer;
+
+public interface CellStyleHandler {
+
+    String execute(Object value, DetailViewerField field, DetailViewerRecord record);
+}
---
+++ trunk/main/src/com/smartgwt/client/widgets/viewer/DetailViewerFieldIfFunction.java
@@ -0,0 +1,6 @@
+package com.smartgwt.client.widgets.viewer;
+
+public interface DetailViewerFieldIfFunction {
+
+    boolean execute(DetailViewer viewer, DetailViewerRecord[] records);
+}
