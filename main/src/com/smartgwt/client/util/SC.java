2c883b2263d19c130f1f72d7b8011247ea189a17
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-02-23 13:24:12 +0000

    issue 48 : support for custom titles for dialog boxes

--- main/src/com/smartgwt/client/util/SC.java
+++ main/src/com/smartgwt/client/util/SC.java
@@ -74,8 +74,20 @@ public class SC {
      * @param message the message
      */
     public static native void say(String message) /*-{
-            $wnd.isc.say(message);
+        $wnd.isc.say(message);
     }-*/;
+
+    /**
+     * Show a modal dialog with a message, icon, and "OK" button. Intended for notifications which are not really warnings (default icon is less severe).
+     *
+     * @param title the title of the message box
+     * @param message the message
+     */
+    public static native void say(String title, String message) /*-{
+	    $wnd.isc.say(message, {title:title});
+	}-*/;
+
+
 
     /**
      * Show a modal dialog with a message, icon, and "OK" button. Intended for notifications which are not really warnings (default icon is less severe).
@@ -85,10 +97,25 @@ public class SC {
      * @param callback the callback to fire when the user dismisses the dialog. 
      */
     public static native void say(String message, BooleanCallback callback) /*-{
-            $wnd.isc.say(message, function(value) {
-                var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
-                callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
-            });
+        $wnd.isc.say(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        });
+    }-*/;
+
+    /**
+     * Show a modal dialog with a message, icon, and "OK" button. Intended for notifications which are not really warnings (default icon is less severe).
+     * The callback will receive boolean true for an OK button click, or null if the Dialog is dismissed via the close button.
+     *
+     * @param title the title of the message box
+     * @param message the message
+     * @param callback the callback to fire when the user dismisses the dialog.
+     */
+    public static native void say(String title, String message, BooleanCallback callback) /*-{
+        $wnd.isc.say(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        }, {title:title});
     }-*/;
 
     /**
@@ -99,39 +126,81 @@ public class SC {
      * @param callback the callback to fire when the user dismisses the dialog.
      */
     public static native void ask(String message, BooleanCallback callback) /*-{
-            $wnd.isc.ask(message, function(value) {
-                var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
-                callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
-            });
+        $wnd.isc.ask(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        });
     }-*/;
 
+    /**
+     * Show a modal dialog with a message, icon, and "Yes" and "No" buttons. The callback will receive boolean true for an OK
+     * button click, boolean false for a No button click, or null if the Dialog is dismissed via the close button.
+     *
+     * @param title the title of the message box
+     * @param message the message
+     * @param callback the callback to fire when the user dismisses the dialog.
+     */
+    public static native void ask(String title, String message, BooleanCallback callback) /*-{
+        $wnd.isc.ask(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        }, {title:title});
+    }-*/;
+
+
     public static native void askforValue(String message, ValueCallback callback) /*-{
-            $wnd.isc.askForValue(message, function(value) {
-                if(value === undefined) value = null;
-                callback.@com.smartgwt.client.util.ValueCallback::execute(Ljava/lang/String;)(value);
-            });
+        $wnd.isc.askForValue(message, function(value) {
+            if(value === undefined) value = null;
+            callback.@com.smartgwt.client.util.ValueCallback::execute(Ljava/lang/String;)(value);
+        });
+    }-*/;
+
+    public static native void askforValue(String title, String message, ValueCallback callback) /*-{
+        $wnd.isc.askForValue(message, function(value) {
+            if(value === undefined) value = null;
+            callback.@com.smartgwt.client.util.ValueCallback::execute(Ljava/lang/String;)(value);
+        }, {title:title});
     }-*/;
 
     public static native void showPrompt(String message) /*-{
-            $wnd.isc.showPrompt(message);
+        $wnd.isc.showPrompt(message);
+    }-*/;
+
+    public static native void showPrompt(String title, String message) /*-{
+        $wnd.isc.showPrompt(message, {title:title});
     }-*/;
 
     public static native void clearPrompt() /*-{
-            $wnd.isc.clearPrompt();
+        $wnd.isc.clearPrompt();
     }-*/;
 
     public static native void confirm(String message, BooleanCallback callback) /*-{
-            $wnd.isc.confirm(message, function(value) {
-                var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
-                callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
-            });
+        $wnd.isc.confirm(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        });
     }-*/;
 
+    public static native void confirm(String title, String message, BooleanCallback callback) /*-{
+	    $wnd.isc.confirm(message, function(value) {
+	        var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+	        callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+	    }, {title:title});
+	}-*/;
+
     public static native void warn(String message, BooleanCallback callback) /*-{
-            $wnd.isc.warn(message, function(value) {
-                var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
-                callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
-            });
+        $wnd.isc.warn(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        });
+    }-*/;
+
+
+     public static native void warn(String title, String message, BooleanCallback callback) /*-{
+        $wnd.isc.warn(message, function(value) {
+            var valueJ = value == null ? null : @com.smartgwt.client.util.JSOHelper::toBoolean(Z)(value);
+            callback.@com.smartgwt.client.util.BooleanCallback::execute(Ljava/lang/Boolean;)(valueJ);
+        }, {title:title});
     }-*/;
 
     /**
