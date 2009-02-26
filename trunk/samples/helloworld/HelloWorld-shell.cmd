7e51d9573cb9c416b87e7ca3ab963b2b65bcd2f4
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-02-26 15:43:59 +0000

    updates

--- trunk/samples/helloworld/HelloWorld-shell.cmd
+++ trunk/samples/helloworld/HelloWorld-shell.cmd
@@ -1,14 +1,9 @@
-if not "%GWT_HOME%" == "" goto checkSmart
+if not "%GWT_HOME%" == "" goto shell
 echo Set the environment variable GWT_HOME to point to you GWT root directory.
-goto end
-
-:checkSmart
-if not "%SMART_GWT_HOME%" == "" goto shell
-echo Set the environment variable SMART_GWT_HOME to point to you SmartGWT root directory.
 goto end
 
 :shell
 
-@java -Xmx500M -cp "%~dp0\src;%~dp0\bin;%GWT_HOME%/gwt-user.jar;%GWT_HOME%/gwt-dev-windows.jar;%SMART_GWT_HOME%/smartgwt.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.mycompany.HelloWorld/HelloWorld.html
+@java -Xmx500M -cp "%~dp0\src;%~dp0\bin;%GWT_HOME%/gwt-user.jar;%GWT_HOME%/gwt-dev-windows.jar;../smartgwt.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.mycompany.HelloWorld/HelloWorld.html
 
 :end
