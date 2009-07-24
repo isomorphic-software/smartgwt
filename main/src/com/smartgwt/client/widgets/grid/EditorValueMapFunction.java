bd1cbe583b5a1bf25204c179188794f9669c4cd8
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-07-24 03:41:24 +0000

    changed EditorValueMapFunction retrun type to Map (value map)

--- main/src/com/smartgwt/client/widgets/grid/EditorValueMapFunction.java
+++ main/src/com/smartgwt/client/widgets/grid/EditorValueMapFunction.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 public interface EditorValueMapFunction {
 
-    String[] getEditorValueMap(Map values, ListGridField field, ListGrid grid);
+    /**
+     * 
+     * @param values Field values for record being edited. Note that this will include the current edit values for fields that have not yet been saved. May be null, if editing a new record.
+     * @param field the ListGridField
+     * @param grid the ListGrid instance
+     * @return value map for the field (stored value --> user displayable value)
+     */
+    Map getEditorValueMap(Map values, ListGridField field, ListGrid grid);
 
 }
