850b5548b3068a77c48f16fbe5d4e3ab5ad2036d
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-08-01 23:55:13 +0000

    added Hover.getCanvas() for being able to manipulate the hover canvas

--- trunk/main/src/com/smartgwt/client/util/Hover.java
+++ trunk/main/src/com/smartgwt/client/util/Hover.java
@@ -16,12 +16,27 @@
 
 package com.smartgwt.client.util;
 
+import com.smartgwt.client.widgets.Canvas;
 
+/**
+ * Utility methods related to hover / tooltips / prompts.
+ */
 public class Hover {
 
+    /**
+     * Clear any active hover prompt.
+     */
     public static native void clear() /*-{
-      return isc.Hover.clear();
-  }-*/;
+      return $wnd.isc.Hover.clear();
+    }-*/;
 
-
+    /**
+     * Return the canvas corresponding to the active hover.
+     *
+     * @return the hover canvas/
+     */
+    public static native Canvas getCanvas() /*-{
+      var canvasJS = $wnd.isc.Hover.hoverCanvas;
+       return canvasJS == null || canvasJS === undefined ? null : @com.smartgwt.client.widgets.Canvas::new(Lcom/google/gwt/core/client/JavaScriptObject;)(canvasJS);
+    }-*/;
 }
