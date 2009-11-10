4e65e030084a03242853e247c6cb4af43441fda7
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2009-11-10 02:14:20 +0000

    handle null enum values

--- trunk/main/src/com/smartgwt/client/util/EnumUtil.java
+++ trunk/main/src/com/smartgwt/client/util/EnumUtil.java
@@ -5,6 +5,7 @@ import com.smartgwt.client.types.ValueEnum;
 public class EnumUtil {
 
     public static <T extends ValueEnum> T getEnum(T[] enums, String value) {
+        if(value == null) return null;
         for (T anEnum : enums) {
             if (anEnum.getValue().equals(value)) {
                 return anEnum;
