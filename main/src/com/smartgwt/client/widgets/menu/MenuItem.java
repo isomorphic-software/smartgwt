dc2646ba1a1adaa88c1cd9d002e9950011c46c2d
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-02-10 22:47:58 +0000

    package import change only

--- main/src/com/smartgwt/client/widgets/menu/MenuItem.java
+++ main/src/com/smartgwt/client/widgets/menu/MenuItem.java
@@ -51,8 +51,7 @@ import java.util.HashMap;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
-import com.smartgwt.client.util.JSOHelper;
-import com.smartgwt.client.util.EnumUtil;
+import com.smartgwt.client.util.*;
 import com.google.gwt.event.shared.*;
 import com.google.gwt.event.shared.HasHandlers;
 
@@ -396,8 +395,6 @@ public class MenuItem extends ListGridRecord  implements com.smartgwt.client.wid
 
 
     // ********************* Static Methods ***********************
-
-
 
 
     /**
