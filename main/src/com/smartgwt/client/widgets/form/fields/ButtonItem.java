0fa12c61d23959ae216185dd8d2bdb943dd7dabe
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-02-10 22:44:06 +0000

    package import change only

--- main/src/com/smartgwt/client/widgets/form/fields/ButtonItem.java
+++ main/src/com/smartgwt/client/widgets/form/fields/ButtonItem.java
@@ -51,8 +51,7 @@ import java.util.HashMap;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
-import com.smartgwt.client.util.JSOHelper;
-import com.smartgwt.client.util.EnumUtil;
+import com.smartgwt.client.util.*;
 import com.google.gwt.event.shared.*;
 import com.google.gwt.event.shared.HasHandlers;
 
@@ -241,10 +240,6 @@ public class ButtonItem extends CanvasItem  implements com.smartgwt.client.widge
 
 
     // ********************* Static Methods ***********************
-
-
-
-
 
 
     public Canvas getCanvas() {
