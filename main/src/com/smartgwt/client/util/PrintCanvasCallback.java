359b5e270277b61bf40971a86ec1481fba2a44d1
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-02-10 20:58:54 +0000

    Callback interfaces for printing APIs PrintProperties object for customizing which controls are automatically included in printing views

---
+++ main/src/com/smartgwt/client/util/PrintCanvasCallback.java
@@ -0,0 +1,8 @@
+package com.smartgwt.client.util;
+
+import com.smartgwt.client.widgets.PrintCanvas;
+
+public interface PrintCanvasCallback {
+
+    void execute(PrintCanvas printCanvas);
+}
---
+++ main/src/com/smartgwt/client/util/PrintHTMLCallback.java
@@ -0,0 +1,6 @@
+package com.smartgwt.client.util;
+
+public interface PrintHTMLCallback {
+
+    void execute(String HTML);
+}
