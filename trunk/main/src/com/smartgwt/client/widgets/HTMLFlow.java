ed30292ab2bc95755bb9541ee6037d3a03f7c79a
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-02-10 22:37:09 +0000

    package import change only

--- trunk/main/src/com/smartgwt/client/widgets/HTMLFlow.java
+++ trunk/main/src/com/smartgwt/client/widgets/HTMLFlow.java
@@ -51,8 +51,7 @@ import java.util.HashMap;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
-import com.smartgwt.client.util.JSOHelper;
-import com.smartgwt.client.util.EnumUtil;
+import com.smartgwt.client.util.*;
 import com.google.gwt.event.shared.*;
 import com.google.gwt.event.shared.HasHandlers;
 
@@ -378,11 +377,6 @@ public class HTMLFlow extends Canvas  implements com.smartgwt.client.widgets.eve
    }-*/;
 
     // ********************* Static Methods ***********************
-
-
-
-
-
 
 
     /**
