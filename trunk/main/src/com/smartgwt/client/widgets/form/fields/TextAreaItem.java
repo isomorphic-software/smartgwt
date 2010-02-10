9a59acd73338defba63c4e2d49ca43d1bc2ebe5d
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-02-10 22:44:06 +0000

    package import change only

--- trunk/main/src/com/smartgwt/client/widgets/form/fields/TextAreaItem.java
+++ trunk/main/src/com/smartgwt/client/widgets/form/fields/TextAreaItem.java
@@ -51,8 +51,7 @@ import java.util.HashMap;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
-import com.smartgwt.client.util.JSOHelper;
-import com.smartgwt.client.util.EnumUtil;
+import com.smartgwt.client.util.*;
 import com.google.gwt.event.shared.*;
 import com.google.gwt.event.shared.HasHandlers;
 
@@ -260,10 +259,6 @@ public class TextAreaItem extends FormItem {
     }-*/;
 
     // ********************* Static Methods ***********************
-
-
-
-
 
 
     /**
