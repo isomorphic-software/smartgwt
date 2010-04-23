f0e06f337a2777287a41fbaafabb46a195d5ec56
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-04-23 13:14:29 +0000

    * AdvancedCriteria: fixes * Criteria.addCriteria: Fixes for when a field criteria is already set. Adding a criteria, when one already exists on the same field, now results in the field criteria set to an array holding all the individual criteria settings for that field.

--- main/src/com/smartgwt/client/data/Criteria.java
+++ main/src/com/smartgwt/client/data/Criteria.java
@@ -22,6 +22,10 @@ import com.smartgwt.client.util.JSOHelper;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Criteria for selecting only a matching set of records from a DataSource. Criteria can be applied on the client and
@@ -43,45 +47,117 @@ public class Criteria extends DataClass {
     public Criteria(JavaScriptObject jsObj) {
         super(jsObj);
     }
-
+    
+    private native boolean fieldIsArray(String field) /*-{
+        var self = this.@com.smartgwt.client.core.DataClass::getJsObj()();
+        return $wnd.isc.isA.Array(self[field]);
+    }-*/;
+    
+    private native boolean fieldIsNull(String field) /*-{
+        var self = this.@com.smartgwt.client.core.DataClass::getJsObj()();
+        return (self[field] == null);
+    }-*/;
+    
     public void addCriteria(String field, String value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            JavaScriptObject array = JSOHelper.getAttributeAsJavaScriptObject(jsObj, field);
+            JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value);
+        } else {
+            String[] newArray = { getAttributeAsString(field), value };
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     public void addCriteria(String field, Integer value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            JavaScriptObject array = JSOHelper.getAttributeAsJavaScriptObject(jsObj, field);
+            JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value.intValue());
+        } else {
+            Integer[] newArray = { JSOHelper.getAttributeAsInt(jsObj, field), value };
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     public void addCriteria(String field, Boolean value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            JavaScriptObject array = JSOHelper.getAttributeAsJavaScriptObject(jsObj, field);
+            JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value.booleanValue());
+        } else {
+            Boolean[] newArray = { JSOHelper.getAttributeAsBoolean(jsObj, field), value };
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     public void addCriteria(String field, Date value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            JavaScriptObject array = JSOHelper.getAttributeAsJavaScriptObject(jsObj, field);
+            JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value);
+        } else {
+            Date[] newArray = { JSOHelper.getAttributeAsDate(jsObj, field), value };
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     public void addCriteria(String field, Float value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            JavaScriptObject array = JSOHelper.getAttributeAsJavaScriptObject(jsObj, field);
+            JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value.floatValue());
+        } else {
+            Float[] newArray = { JSOHelper.getAttributeAsFloat(jsObj, field), value };
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     public void addCriteria(String field, String[] value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            ArrayList<String> newArray = new ArrayList<String>();
+            newArray.addAll(Arrays.asList(JSOHelper.getAttributeAsStringArray(jsObj, field)));
+            newArray.addAll(Arrays.asList(value));
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray.toArray()));
+        } else {
+            ArrayList<String> newArray = new ArrayList<String>();
+            newArray.add(getAttributeAsString(field));
+            newArray.addAll(Arrays.asList(value));
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray.toArray()));
+        }
     }
 
     public void addCriteria(String field, Integer[] value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            int[] oldArray = JSOHelper.getAttributeAsIntArray(jsObj, field);
+            Integer[] newArray = new Integer[oldArray.length+value.length];
+            for (int i=0; i<oldArray.length; i++) newArray[i] = new Integer(oldArray[i]);
+            for (int i=0; i<value.length; i++) newArray[i+oldArray.length] = value[i];
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        } else {
+            Integer[] newArray = new Integer[value.length+1];
+            newArray[0] = new Integer(JSOHelper.getAttributeAsInt(jsObj, field));
+            for (int i=0; i<value.length; i++) newArray[i+1] = value[i];
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
-    public void addCriteria(String field, Boolean[] value) {
-        JSOHelper.setAttribute(jsObj, field, value);
-    }
-
-    public void addCriteria(String field, Date[] value) {
-        JSOHelper.setAttribute(jsObj, field, value);
-    }
-
-    public void addCriteria(String field, Float[] value) {
-        JSOHelper.setAttribute(jsObj, field, value);
+    public void addCriteria(String field, Double[] value) {
+        if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+        else if (fieldIsArray(field)) {
+            double[] oldArray = JSOHelper.getAttributeAsDoubleArray(jsObj, field);
+            Double[] newArray = new Double[oldArray.length+value.length];
+            for (int i=0; i<oldArray.length; i++) newArray[i] = new Double(oldArray[i]);
+            for (int i=0; i<value.length; i++) newArray[i+oldArray.length] = value[i];
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        } else {
+            Double[] newArray = new Double[value.length+1];
+            newArray[0] = new Double(JSOHelper.getAttributeAsDouble(jsObj, field));
+            for (int i=0; i<value.length; i++) newArray[i+1] = value[i];
+            setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+        }
     }
 
     /**
@@ -90,7 +166,33 @@ public class Criteria extends DataClass {
      * @param otherCriteria the passed criteria object
      */
     public void addCriteria(Criteria otherCriteria) {
-        JSOHelper.addProperties(jsObj, otherCriteria.getJsObj());
+        Map otherMap = otherCriteria.getValues();
+        Set otherKeys = otherMap.keySet();
+        for (Iterator i=otherKeys.iterator(); i.hasNext(); ) {
+            String field = (String) i.next();
+            Object value = otherMap.get(field);
+
+            if (value instanceof Integer) {
+                addCriteria(field, (Integer) value);
+            } else if (value instanceof Float) {
+                addCriteria(field, (Float) value);
+            } else if (value instanceof String) {
+                addCriteria(field, (String) value);
+            } else if (value instanceof Date) {
+                addCriteria(field, (Date) value);
+            } else if (value instanceof Boolean) {
+                addCriteria(field, (Boolean) value);
+            } else {
+                if (fieldIsNull(field)) JSOHelper.setAttribute(jsObj, field, value);
+                else if (fieldIsArray(field)) {
+                    JavaScriptObject array = getAttributeAsJavaScriptObject(field);
+                    JSOHelper.setArrayValue(array, JSOHelper.arrayLength(array), value);
+                } else {
+                    Object[] newArray = { getAttributeAsObject(field), value };
+                    setAttribute(field, JSOHelper.convertToJavaScriptArray(newArray));
+                }
+            }
+        }
     }
 
     /**
