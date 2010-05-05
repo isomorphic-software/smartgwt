0f284f6f17450f171e33f1389f852cd19a5c4280
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-05-05 01:33:32 +0000

    * treat FormItem height / width of 100% as "*"

--- main/src/com/smartgwt/client/widgets/form/fields/FormItem.java
+++ main/src/com/smartgwt/client/widgets/form/fields/FormItem.java
@@ -3519,12 +3519,13 @@ public class FormItem extends RefDataClass  implements com.smartgwt.client.widge
     }
 
     public void setWidth(String width) {
-        assert width.indexOf("100%") == -1 : "Use setWidth(\"*\") rather than setWidth(\"100%\")";
+        if("100%".equals(width)) width = "*";
         assert width.indexOf("%") == -1 : "FormItems do not support percent sizing.";
         setAttribute("width", width);
     }
 
     public void setHeight(String height) {
+        if("100%".equals(height)) height = "*";
         assert height.indexOf("%") == -1 : "FormItems do not support percent sizing.";
         setAttribute("height", height);
     }
