970a8809e7bb71027a49085bb3709e56e97ad23c
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-05-08 02:21:30 +0000

    * doc updates only

--- trunk/main/src/com/smartgwt/client/docs/Mail.java
+++ trunk/main/src/com/smartgwt/client/docs/Mail.java
@@ -3,17 +3,17 @@ package com.smartgwt.client.docs;
 
 /**
  * @see com.smartgwt.client.types.Mail
- * @see com.smartgwt.client.data.OperationBinding#getMail
- * @see com.smartgwt.client.types.Mail#getTemplateFile
+ * @see com.smartgwt.client.docs.serverds.OperationBinding#mail
+ * @see com.smartgwt.client.docs.serverds.Mail#templateFile
  * @see com.smartgwt.client.types.Mail#getMessageTemplate
- * @see com.smartgwt.client.types.Mail#getTo
- * @see com.smartgwt.client.types.Mail#getCc
- * @see com.smartgwt.client.types.Mail#getBcc
- * @see com.smartgwt.client.types.Mail#getFrom
- * @see com.smartgwt.client.types.Mail#getReplyTo
- * @see com.smartgwt.client.types.Mail#getSubject
- * @see com.smartgwt.client.types.Mail#getMultiple
- * @see com.smartgwt.client.types.Mail#getMessageData
+ * @see com.smartgwt.client.docs.serverds.Mail#to
+ * @see com.smartgwt.client.docs.serverds.Mail#cc
+ * @see com.smartgwt.client.docs.serverds.Mail#bcc
+ * @see com.smartgwt.client.docs.serverds.Mail#from
+ * @see com.smartgwt.client.docs.serverds.Mail#replyTo
+ * @see com.smartgwt.client.docs.serverds.Mail#subject
+ * @see com.smartgwt.client.docs.serverds.Mail#multiple
+ * @see com.smartgwt.client.docs.serverds.Mail#messageData
  */
 public interface Mail {
 }
