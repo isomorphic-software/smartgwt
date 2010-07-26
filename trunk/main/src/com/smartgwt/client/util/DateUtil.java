73fe78a5eb0a49b9d2f45c67f61c0f556b61385d
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-07-26 19:06:21 +0000

    Added support for disabling Daylight Savings Time adjustments for time / datetime type values

--- trunk/main/src/com/smartgwt/client/util/DateUtil.java
+++ trunk/main/src/com/smartgwt/client/util/DateUtil.java
@@ -12,6 +12,9 @@ public class DateUtil {
      *
      * If this method is never called, the default display timezone for times and datetimes will be derived from the native
      * browser local timezone.
+     * <p>
+     * Note that by default daylight savings time adjustments (based on browser locale) may also be applied when formatting times
+     * and datetimes. {@link #setAdjustForDST(boolean)} may be used to disable this adjustment.
      *
      * @param offset offset from UTC. This should be a string in the format <code>+/-HH:MM</code> for example <code>"-08:00"
      */
@@ -19,6 +22,23 @@ public class DateUtil {
         $wnd.isc.Time.setDefaultDisplayTimezone(offset);
     }-*/;
 
+    /**
+     * 
+     * Determines whether, when formatting values of type datetime and time, 
+     * the effect of Daylight Saving Time should be considered when computing offsets from UTC.
+     * By default, this flag is set during framework initialization if SmartGWT detects that
+     * it is running in a locale that is observing DST this year.
+     * If you do not want DST adjustments to be applied, set this flag to false.<p>
+     * Note that setting this flag to true will have no effect unless you are in a locale 
+     * that is observing Daylight Saving Time this year; this is because we rely on the
+     * browser for offset information, and browsers are only capable of returning local date
+     * and time information for the computer's current locale.
+     * 
+     * @param whether time and datetimes should account for daylight savings time in this application
+     */
+    public native void setAdjustForDST(boolean adjustForDST) /*-{
+		$wnd.isc.Time.adjustForDST = adjustForDST;
+	}-*/;
 
     /**
      * Set the default formatter for date objects to the method name passed in. After calling this method, subsequent calls to Date.toNormalDate will return a string formatted according to this format specification.
