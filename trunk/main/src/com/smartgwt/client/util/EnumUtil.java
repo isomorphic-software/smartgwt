8b1a4f76982759c47ce6b07cffccacc20bac49e0
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-09-01 13:56:56 +0000

    New EnumUtil method for handling arrays of enums

--- trunk/main/src/com/smartgwt/client/util/EnumUtil.java
+++ trunk/main/src/com/smartgwt/client/util/EnumUtil.java
@@ -13,4 +13,19 @@ public class EnumUtil {
         }
         return null;
     }
+
+    public static <T extends ValueEnum> ValueEnum[] getEnums(T[] enums, String[] values) {
+        if(values == null) return null;
+        ValueEnum ret[] = new ValueEnum[values.length];
+        for (int i=0; i<values.length; i++) {
+            String value = values[i];
+            ret[i] = null;
+            for (T anEnum : enums) {
+                if (anEnum.getValue().equals(value)) {
+                    ret[i] = (ValueEnum) anEnum;
+                }
+            }
+        }
+        return ret;
+    }
 }
