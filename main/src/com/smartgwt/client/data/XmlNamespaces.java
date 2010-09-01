02dbd757e481a64cc90c0f32a5c843416fd29517
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-09-01 13:57:35 +0000

    Added methods to support SmartGWT EE code.

--- main/src/com/smartgwt/client/data/XmlNamespaces.java
+++ main/src/com/smartgwt/client/data/XmlNamespaces.java
@@ -1,8 +1,15 @@
 package com.smartgwt.client.data;
 
 import com.smartgwt.client.core.DataClass;
+import com.google.gwt.core.client.JavaScriptObject;
 
 public class XmlNamespaces extends DataClass {
+
+    public XmlNamespaces(){}
+
+    public XmlNamespaces(JavaScriptObject jsObj){
+        super(jsObj);
+    }
 
     public void addNamespace(String prefix, String url) {
         setAttribute(prefix, url);
