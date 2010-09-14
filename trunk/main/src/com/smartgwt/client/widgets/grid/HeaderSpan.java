dacecdb8823bccd7b098509b210744843b0305bb
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-09-14 02:56:04 +0000

    Added ListGrid.getHeaderSpanContextMenuItems(HeaderSpan headerSpan) override point to allow header span contenxt menu customizations

--- trunk/main/src/com/smartgwt/client/widgets/grid/HeaderSpan.java
+++ trunk/main/src/com/smartgwt/client/widgets/grid/HeaderSpan.java
@@ -1,10 +1,15 @@
 package com.smartgwt.client.widgets.grid;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.smartgwt.client.core.DataClass;
 
 public class HeaderSpan extends DataClass {
 
     public HeaderSpan() {
+    }
+
+    public HeaderSpan(JavaScriptObject jsObj) {
+        super(jsObj);
     }
 
     public HeaderSpan(String title, String[] fields) {
@@ -16,8 +21,15 @@ public class HeaderSpan extends DataClass {
         setAttribute("title", title);
     }
 
+    public String getTitle() {
+        return getAttribute("title");
+    }
+
     public void setFields(String... fields) {
         setAttribute("fields", fields);
     }
 
+    public String[] getFields() {
+        return getAttributeAsStringArray("fields");
+    }
 }
