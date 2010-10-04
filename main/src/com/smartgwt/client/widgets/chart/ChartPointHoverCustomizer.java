91bde7cf862129e6ed81b3673386bd91bc69d499
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-10-04 23:24:21 +0000

    Added FacetChart and Interfaces for chart point events

---
+++ main/src/com/smartgwt/client/widgets/chart/ChartPointHoverCustomizer.java
@@ -0,0 +1,8 @@
+package com.smartgwt.client.widgets.chart;
+
+import com.smartgwt.client.data.Record;
+
+public interface ChartPointHoverCustomizer {
+	
+	 String hoverHTML(Float value, Record record);
+}
