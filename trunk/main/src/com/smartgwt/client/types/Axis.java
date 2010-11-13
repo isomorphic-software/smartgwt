665a605af5f9b1574e4a7ba4f637b67019534bc6
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-11-13 00:15:07 +0000

    New enumerated types for use by CubeGrids

---
+++ trunk/main/src/com/smartgwt/client/types/Axis.java
@@ -0,0 +1,30 @@
+package com.smartgwt.client.types;
+
+/**
+ * An axis or "side" of a table
+ *
+ */
+public enum Axis implements ValueEnum {
+
+	/**
+	 * Row axis
+	 */
+	ROW("row"),
+	
+	/**
+	 * Column axis
+	 */
+	COLUMN("column");
+	
+	private String value;
+
+	Axis(String value) {
+	    this.value = value;
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+
+}
---
+++ trunk/main/src/com/smartgwt/client/types/SelectionBoundary.java
@@ -0,0 +1,35 @@
+package com.smartgwt.client.types;
+
+/**
+ * Boundary type for limiting where contiguous selection (via shift+click or drag selection)
+ * can be applied across {@link com.smartgwt.client.widgets.cube.Facet#setSelectionBoundary,facets}
+ * or {@link com.smartgwt.client.widgets.cube.FacetValue#setSelectionBoundary(),facetValues}
+ *
+ */
+public enum SelectionBoundary implements ValueEnum {
+	/**
+	 * selection boundary applies to the bottom / right of the cells
+	 */
+	AFTER("after"),
+	/**
+	 * selection boundary applies to the top / left of the cells
+	 */
+	BEFORE("before"),
+	/**
+	 * selection boundary applies to both edges.
+	 */
+	BOTH("both");
+	
+	private String value;
+	SelectionBoundary(String value) {
+		this.value = value;
+	}
+	
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
