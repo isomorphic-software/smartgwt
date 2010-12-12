5e4c06072dc548ae3176a95e184c7479a818abd2
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-12-12 23:37:25 +0000

    - issue 540 : add support for ListGrid groupBy event handler - added ListGrid.setShowBackgroundComponent(boolean) - other enhancements

--- trunk/main/src/com/smartgwt/client/widgets/grid/events/HasGroupByHandlers.java
+++ trunk/main/src/com/smartgwt/client/widgets/grid/events/HasGroupByHandlers.java
@@ -20,17 +20,12 @@ import com.smartgwt.client.event.*;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.HasHandlers;
 
-public interface HasDrawAreaChangedHandlers extends HasHandlers {
+public interface HasGroupByHandlers extends HasHandlers {
     /**
-     * Notification method that fires when the drawArea changes due to scrolling.  Receives   the previous drawArea
-     * co-ordinates as parameters.  Call {@link com.smartgwt.client.widgets.grid.ListGrid#getDrawArea ListGrid.getDrawArea} to 
-     * get the new drawArea co-ordinates. <P> Note that if this grid is showing any {@link
-     * com.smartgwt.client.widgets.grid.ListGridField#getFrozen frozen fields}, they will not be included in the
-     * <code>oldStartCol</code>, <code>oldEndCol</code> range reported by this method. Frozen fields are assumed never to be
-     * scrolled out of view.
+     * Callback fired when the listGrid is grouped or ungrouped.  <var class="SmartGwt">This event may be cancelled</var>
      *
-     * @param handler the drawAreaChanged handler
+     * @param handler the groupBy handler
      * @return {@link HandlerRegistration} used to remove this handler
      */
-    HandlerRegistration addDrawAreaChangedHandler(DrawAreaChangedHandler handler);
+    HandlerRegistration addGroupByHandler(GroupByHandler handler);
 }
