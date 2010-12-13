e2a6b40c86293c01a14dd36658ab0c1fd8ffaa43
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-12-13 02:28:34 +0000

    support for hidden validation error handler for DynamicFor and ValuesManger. Made some other fixes as well

---
+++ main/src/com/smartgwt/client/widgets/form/events/HasHiddenValidationErrorsHandlers.java
@@ -0,0 +1,35 @@
+/*
+ * Smart GWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * Smart GWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  Smart GWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+ 
+package com.smartgwt.client.widgets.form.events;
+
+import com.smartgwt.client.event.*;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
+
+public interface HasHiddenValidationErrorsHandlers extends HasHandlers {
+    /**
+     * Method to display validation error messages for fields that are not currently visible  in this form.<br> This will be
+     * called when validation fails for<br> - a hidden field in this form<br> - if this form is databound, a datasource field
+     * with specified validators, for which we   have no specified form item.<br> Implement this to provide custom validation
+     * error handling for these fields.<br> By default hidden validation errors will be logged as warnings in the
+     * developerConsole. Return false from this method to suppress that behavior.
+     *
+     * @param handler the hiddenValidationErrors handler
+     * @return {@link HandlerRegistration} used to remove this handler
+     */
+    HandlerRegistration addHiddenValidationErrorsHandler(HiddenValidationErrorsHandler handler);
+}
