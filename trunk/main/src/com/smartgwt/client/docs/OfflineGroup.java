745ab2097a90a60db288b7e6b9f2037de07295f6
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2010-12-19 16:00:33 +0000

    javadoc updates

--- trunk/main/src/com/smartgwt/client/docs/OfflineGroup.java
+++ trunk/main/src/com/smartgwt/client/docs/OfflineGroup.java
@@ -7,6 +7,7 @@ package com.smartgwt.client.docs;
  * @see com.smartgwt.client.util.Offline
  * @see com.smartgwt.client.data.DataSource#offlineMessage
  * @see com.smartgwt.client.rpc.RPCResponse#STATUS_OFFLINE
+ * @see com.smartgwt.client.widgets.grid.ListGrid#getOfflineMessageStyle
  * @see com.smartgwt.client.data.DataSource#getUseOfflineStorage
  * @see com.smartgwt.client.data.DSResponse#getOfflineTimestamp
  * @see com.smartgwt.client.data.DSResponse#getFromOfflineCache
