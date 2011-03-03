23dcd360f088fc43d9bbdfb6d7c61d0fb725df1e
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-03-03 00:04:41 +0000

    Beefed up DateUtil somewhat significantly: - Added setShortDatetimeDisplayFormat and setShortDatetimeDisplayFormatter in addition to setShortDateDisplayFormat / formatter   this allows customization of default display of datetime fields - added wrappers around the SmartClient provided Date utility methods to parse a string to a date using the   standard input format, and format a Date to a string (normal date, short date, short datetime) using the   standard specified formatters - added a method to customize the standard input format - deprecated the (poorly named) setDateInputFormatter() in favor of setDateParser()
    Also cleaned up the javadocs to make these APIs clearer and easier to understand

--- main/src/com/smartgwt/client/util/DateUtil.java
+++ main/src/com/smartgwt/client/util/DateUtil.java
@@ -1,5 +1,7 @@
 package com.smartgwt.client.util;
 
+import java.util.Date;
+
 import com.smartgwt.client.types.DateDisplayFormat;
 
 /**
@@ -101,10 +103,16 @@ public class DateUtil {
     }-*/;
 
     /**
-     * Set the default formatter for date objects to the method name passed in. After calling this method, subsequent calls to Date.toShortDate will return a string formatted according to this format specification.
-     * <br>
-     * <b>Note</b>: this will be the standard long date format used by SmartGWT components.
-     * Initial default normalDisplayFormat is "toUSShortDate"
+     * Set the system wide default short date format.
+     * The specified format will be used by default by SmartGwt components when formatting date values to 
+     * short date format (and by {@link #formatAsShortDate(Date)}).
+     * <P>
+     * This method, together with {@link DateUtil#setDefaultDateSeparator(String)} provide support for most
+     * standard "short date" formats. However if a custom format which doesn't match any of the specified
+     * {@link DateDisplayFormat} types is required, a custom formatting function may be provided via
+     * {@link #setShortDateDisplayFormatter(DateDisplayFormatter)}.
+     * <P>
+     * Note that the default short date format is initially set to "toUSShortDate".
      *
      * @param format the DateDisplayFormat
      */    
@@ -114,12 +122,21 @@ public class DateUtil {
 
 
     /**
-     * Set the default formatter for date objects to the method name passed in. After calling this method, subsequent calls to Date.toShortDate will return a string formatted according to this format specification.
-     * <br>
-     * <b>Note</b>: this will be the standard long date format used by SmartGWT components.
-     * The DateDisplayFormatter function will be executed in the scope of the Date and should return the formatted string.
-     * <p/>
-     * Initial default normalDisplayFormat is "toUSShortDate"
+     * Set up a system wide default short date formatting function.
+     * The formatter passed in will be used by default by SmartGwt components when formatting date values
+     * to short date format (and by {@link #formatAsShortDate(Date)}).
+     * <P>
+     * Note that {@link DateUtil#setShortDateDisplayFormat(DateDisplayFormat)} 
+     * and {@link DateUtil#setDefaultDateSeparator(String)} already provide support for most standard "short date"
+     * formats without the need for a custom formatter.
+     * <P>
+     * If a custom short date formatter is applied, bear in mind that it will be applied by default when
+     * editing date values, so the system will need to be able to parse an edited date string in this format
+     * back to a live date object. Developers calling this method will therefore also commonly want to
+     * apply custom parsing logic via {@link #setDateInputFormat(String)} or {@link #setDateParser(DateParser)}.
+     * <P>
+     * When writing custom date formatting and parsing logic, developers may find the 
+     * {@link com.google.gwt.i18n.client.DateTimeFormat} class helpful.
      *
      * Sample code :
      * <pre>
@@ -143,9 +160,132 @@ public class DateUtil {
             }
         );
     }-*/;
+    
+    /**
+     * Set the system wide default short datetime format.
+     * The specified format will be used by default by SmartGwt components when formatting datetime field values to 
+     * short datetime format (and by {@link #formatAsShortDateTime(Date)}).
+     * <P>
+     * This method, together with {@link DateUtil#setDefaultDateSeparator(String)} provide support for most
+     * standard "short date" formats. However if a custom format which doesn't match any of the specified
+     * {@link DateDisplayFormat} types is required, a custom formatting function may be provided via
+     * {@link #setShortDatetimeDisplayFormatter(DateDisplayFormatter)}.
+     * <P>
+     * Note that the default short datetime format is initially set to "toUSShortDateTime".
+     *
+     * @param format the DateDisplayFormat
+     */    
+    public static native void setShortDatetimeDisplayFormat(DateDisplayFormat format) /*-{
+        $wnd.Date.setShortDatetimeDisplayFormat(format.@com.smartgwt.client.types.DateDisplayFormat::getValue()());
+    }-*/;
+
 
     /**
-     * Sets up the default format for strings being parsed into dates via Date.parseInput
+     * Set up a system wide default short datetime formatting function.
+     * The formatter passed in will be used by default by SmartGwt components when formatting date values
+     * to short datetime format (and by {@link #formatAsShortDatetime(Date)}).
+     * <P>
+     * Note that {@link DateUtil#setShortDatetimeDisplayFormat(DateDisplayFormat)} 
+     * and {@link DateUtil#setDefaultDateSeparator(String)} already provide support for most standard "short date"
+     * formats without the need for a custom formatter.
+     * <P>
+     * If a custom short datetime formatter is applied, bear in mind that it will be applied by default when
+     * editing date values, so the system will need to be able to parse an edited date string in this format
+     * back to a live date object. Developers calling this method will therefore also commonly want to
+     * apply custom parsing logic via {@link #setDateInputFormat(String)} or {@link #setDateParser(DateParser)}.
+     * <P>
+     * When writing custom date formatting and parsing logic, developers may find the 
+     * {@link com.google.gwt.i18n.client.DateTimeFormat} class helpful.
+     *
+     * @param formatter the DateDisplayFormatter
+     */
+    public static native void setShortDatetimeDisplayFormatter(DateDisplayFormatter formatter) /*-{
+        $wnd.Date.setShortDatetimeDisplayFormat(function() {
+                var date = this;
+                var dateJ = date == null || date === undefined ? null : @com.smartgwt.client.util.JSOHelper::toDate(D)(date.getTime());
+                return formatter.@com.smartgwt.client.util.DateDisplayFormatter::format(Ljava/util/Date;)(dateJ);
+            }
+        );
+    }-*/;
+    
+    // Wrappers for the standard Date format / parser functions applied to the Date class in SC.
+    // Note that GWT has date parsing and formatting functions via Java util classes - just exposing these
+    // for completeness as they're the standards used by components by default
+    
+    /**
+     * Parse a formatted date string into a Date object.
+     * <P>
+     * This calls the default parsing function used by SmartGWT components.
+     * <P>
+     * If {@link #setDateInputFormat(String)} or {@link #setDateParser(DateParser)} 
+     * has been specified it will be respected.
+     * 
+     * @param dateString string to parse into a Date
+     * @return date value from the string (or null if the string passed in could not be converted to a date).
+     */
+    public static native Date parseInput(String dateString) /*-{
+        var jsDate = $wnd.Date.parseInput(dateString);
+        if (jsDate == null) return null;
+        return @com.smartgwt.client.util.JSOHelper::toDate(D)(jsDate.getTime());
+        
+    }-*/; 
+
+    /**
+     * Format a date as a string according to the format specified by
+     * {@link #setShortDateDisplayFormat(DateDisplayFormat)} or
+     * {@link #setShortDateDisplayFormatter(DateDisplayFormatter)}.
+     * <P>
+     * This calls the standard date formatting function used by SmartGWT components to display short-formatted
+     * dates.
+     * 
+     * @param date
+     * @return
+     */
+    public static native String formatAsShortDate(Date date) /*-{
+        if (date == null) return "";
+        var jsDate = @com.smartgwt.client.util.JSOHelper::convertToJavaScriptDate(Ljava/util/Date;)(date);
+        return jsDate.toShortDate();
+        
+    }-*/;
+    
+    /**
+     * Format a date as a string according to the format specified by
+     * {@link #setShortDatetimeDisplayFormat(DateDisplayFormat)} or
+     * {@link #setShortDatetimeDisplayFormatter(DateDisplayFormatter)}.
+     * <P>
+     * This calls the standard date formatting function used by SmartGWT components to display short-formatted
+     * date-times.
+     * 
+     * @param date
+     * @return
+     */
+    public static native String formatAsShortDatetime(Date date) /*-{
+        if (date == null) return "";
+        var jsDate = @com.smartgwt.client.util.JSOHelper::convertToJavaScriptDate(Ljava/util/Date;)(date);
+        return jsDate.toShortDateTime();
+        
+    }-*/;
+    
+    /**
+     * Format a date as a string according to the format specified by
+     * {@link #setNormalDateDisplayFormat(DateDisplayFormat)} or
+     * {@link #setNormalDateDisplayFormatter(DateDisplayFormatter)}.
+     * <P>
+     * This calls the standard date formatting function used by SmartGWT components to display short-formatted
+     * dates.
+     * 
+     * @param date
+     * @return
+     */
+    public static native String format(Date date) /*-{
+        if (date == null) return "";
+        var jsDate = @com.smartgwt.client.util.JSOHelper::convertToJavaScriptDate(Ljava/util/Date;)(date);
+        return jsDate.toShortDate();
+        
+    }-*/;
+    
+    /**
+     * Sets up the default format for strings being parsed into dates via DateUtil.parseInput
      * <br>
      * Sample code :
      * <pre>
@@ -159,6 +299,7 @@ public class DateUtil {
      * </pre>
      *
      * @param formatter the DateInputFormatter
+     * @deprecated in favor of setDateParser()
      */
     public static native void setDateInputFormatter(DateInputFormatter formatter) /*-{
         $wnd.Date.setInputFormat(function(dateStr) {
@@ -167,5 +308,82 @@ public class DateUtil {
                 return @com.smartgwt.client.util.JSOHelper::toDateJS(Ljava/util/Date;)(dateJ);
             }
         );
+    }-*/;
+    
+    /**
+     * Sets up the a custom parsing function to use by default when converting dates or datetimes from 
+     * formatted string values to Dates. This custom parser will be used by SmartGwt components
+     * parsing editable date or datetime type values back to live dates by default.
+     * The string passed in will be formatted according to the standard "short date" 
+     * or "short datetime" format (which may be customized via 
+     * {@link #setShortDateDisplayFormat(DateDisplayFormat)} or
+     * {@link #setShortDateDisplayFormatter(DateDisplayFormatter)} and
+     * {@link #setShortDatetimeDisplayFormat(DateDisplayFormat)} or
+     * {@link #setShortDatetimeDisplayFormatter(DateDisplayFormatter)} methods.
+     * <P>
+     * Note that the default date parsing logic already handles all standard short date formats, including
+     * those formatted with custom separators. In most cases rather than applying an entirely custom
+     * date parser method, desired behavior can be achieved via changing the
+     * standard {@link #setDateInputFormat(String),input format}.
+     * <P>
+     * When writing custom date formatting and parsing logic, developers may find the 
+     * {@link com.google.gwt.i18n.client.DateTimeFormat} class helpful.
+     * <P>
+     * Sample code :
+     * <pre>
+     * DateUtil.setDateParser(new DateParser() {
+     *    public Date parse(String dateString) {
+     *       final DateTimeFormat format = DateTimeFormat.getFormat("MMM d, yyyy");
+     *       Date date = format.parse(dateString);
+     *       return date;
+     *    }
+     * });
+     * </pre>
+     * <P>
+     * Individual components may also override date formatting and parsing functions directly.
+     * 
+     * @param parser
+     */
+    public static native void setDateParser(DateParser parser) /*-{
+        $wnd.Date.setInputFormat(function(dateStr) {
+                var dateJ = formatter.@com.smartgwt.client.util.DateParser::parse(Ljava/lang/String;)(dateStr);
+                if(dateJ == null) return null;
+                return @com.smartgwt.client.util.JSOHelper::toDateJS(Ljava/util/Date;)(dateJ);
+            }
+        );
+    }-*/;
+    
+    /**
+     * Sets up the default system-wide input format for strings being parsed into dates via SmartGWT
+     * utilities and components (see {@link #parseInput(String)}). This input format is respected when
+     * parsing formatted strings to "date" or "datetime" type values.
+     * <P>
+     * This method takes a 3 character string like <code>"MDY"</code> indicating the 
+     * order of the Month, Day and Year components of date strings.
+     * <P>
+     * As an example - an input format of "MDY" would parse "01/02/1999" to Jan 2nd 1999<br>
+     * This standard parsing logic will also handle date-time strings such as "01/02/1999 08:45", or
+     * "01/02/1999 16:21:05". 
+     * <P>
+     * Notes:
+     * <ul>
+     * <li>If this method is never called, the system attempts to automatically determine the standard input
+     *     format will be automatically determined based on the specified 
+     *     {@link #setShortDateDisplayFormat(DateDisplayFormat),short date display format}. For 
+     *     example if the short display format has been set to "toEuropeanShortDate" the input format will default
+     *     to "DMY".</li>
+     * <li>The default date parsing functionality built into SmartGWT will handle dates presented
+     *     with any separator string, and can 1 or 2 digit day and month values and 2 or 4 digit year values. This
+     *     means that in many cases custom date display formats can be parsed back to Date values without the need
+     *     for explicit custom formatter functions. However if more sophisticated parsing logic is required an
+     *      entirely custom parser function may be applied via {@link setDateParser()}.</li>
+     * <li>Date parsing and formatting logic may be overridden at the component level by setting properties 
+     *     directly on the component or field in question.</li>
+     * </ul>
+     * 
+     * @param inputFormat 
+     */
+    public static native void setDateInputFormat(String inputFormat) /*-{
+        $wnd.Date.setInputFormat(inputFormat);
     }-*/;
 }
