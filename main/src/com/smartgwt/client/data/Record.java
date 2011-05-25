6da850d77e2f3b9796a78080d3b598d029ef7947
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-05-25 23:51:25 +0000

    Clarified switching a Record to other wrapper types.

--- main/src/com/smartgwt/client/data/Record.java
+++ main/src/com/smartgwt/client/data/Record.java
@@ -23,21 +23,33 @@ import com.smartgwt.client.util.JSOHelper;
 import com.google.gwt.core.client.JavaScriptObject;
 
 /**
- * A Record is an ordinary Java class with properties that are treated as data to be displayed and edited by a
- * {@link com.smartgwt.client.widgets.DataBoundComponent}.
+ * A Record contains attributes that are displayed and edited by a {@link com.smartgwt.client.widgets.DataBoundComponent}.
  * <p/>
  * {@link com.smartgwt.client.widgets.DataBoundComponent}s have a concept of named fields, where values for each field
- * are found under the same-named property in a Record.
- * <p/>
- * A Record is always an ordinary Java object regardless of how the record is loaded (static data, java server, XML web service, etc).
+ * are found under the same-named attribute in a Record.
  * <p/>
  * The concept of working with Records is common to all {@link com.smartgwt.client.widgets.DataBoundComponent}s, although individual
  * DataBoundComponents may work with singular records ({@link com.smartgwt.client.widgets.form.DynamicForm}) or may work with lists
  * ({@link com.smartgwt.client.widgets.grid.ListGrid}), trees ({@link com.smartgwt.client.widgets.tree.TreeGrid}), or cubes
  * (CubeGrid) of records.
  * <p/>
- * <p>Individual DataComponents may also look for special properties on Records which control styling or behavior for those records,
- * such as {@link com.smartgwt.client.widgets.grid.ListGrid#setRecordEditProperty(String)}
+ * A Record is always the same type of Java object regardless of how the record is loaded (static
+ * data, java server, XML web service, etc).
+ * <p/>
+ * Individual DataBoundComponents may also look for special attributes on Records which control
+ * styling or behavior.  For convenience, there are subclasses of Record with type-safe setters
+ * for such attributes (such as {@link
+ * com.smartgwt.client.widgets.grid.ListGrid#setRecordEditProperty(String)}).  In reality, all
+ * such subclasses are wrappers over the same underlying data object, and you can convert to
+ * whichever wrapper is most convenient via:
+ * <pre>
+ *    new ListGridRecord(recordInstance.getJsObj());
+ * </pre>
+ * You can also create your own subclass of Record with type-specific getters and setters,
+ * however, if you do so, you should store values via setAttribute() and retrieve them via
+ * getAttribute() rather than keeping values as normal Java properties.  Only attributes will
+ * be visible to DataBoundComponents, ordinary Java properties will not. 
+ * <p/>
  */
 public class Record extends RefDataClass {
 
