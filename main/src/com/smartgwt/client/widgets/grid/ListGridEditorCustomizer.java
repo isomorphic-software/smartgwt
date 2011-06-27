f6f0b5c996c0ec7b15dd56d55055c318f7708797
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-06-27 17:11:31 +0000

    Helper classes for wrapping the ListGrid getEditorProperties method for SGWT for per-cell edit-item customization

---
+++ main/src/com/smartgwt/client/widgets/grid/ListGridEditorCustomizer.java
@@ -0,0 +1,22 @@
+package com.smartgwt.client.widgets.grid;
+
+import com.smartgwt.client.widgets.form.fields.FormItem;
+
+/**
+ * Customizer for the edit FormItem shown in a ListGrid cell
+ */
+public interface ListGridEditorCustomizer {
+    /**
+     * This method fires when the editor for a cell in an editable grid is being displayed, and allows
+     * developers to customize the FormItem displayed to the user dynamically depending on the row being edited
+     * as well as just the field. The FormItem returned will be used as
+     * a template (properties block) for the item displayed to the user - as with {@link ListGridField#setEditorType()} 
+     * if applying event handlers to the item, the live item should be retrieved from the event object.
+     * <P>
+     * For default behavior simply return the result of {@link ListGridEditorContext#getDefaultProperties()}.    
+     * @param context context object containing details of the cell being edited.
+     * @return template FormItem.
+     */
+    public FormItem getEditor(ListGridEditorContext context);
+
+}
