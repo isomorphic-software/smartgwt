6be56991c1e99effce428e1c0195342ac9665872
sanjiv.jivan <sanjiv.jivan@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-06-28 03:02:18 +0000

    polishing

--- trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/forms/ListGridItemSample.java
+++ trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/forms/ListGridItemSample.java
@@ -1,3 +1,19 @@
+/*
+ * Smart GWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * Smart GWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * is published by the Free Software Foundation.  Smart GWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+
 package com.smartgwt.sample.showcase.client.forms;
 
 import java.util.HashMap;
@@ -170,7 +186,7 @@ public class ListGridItemSample extends ShowcasePanel {
         final DynamicForm exampleForm = new DynamicForm();
         exampleForm.setLeft(200);
         exampleForm.setCanDragResize(true);
-        exampleForm.setBorder("2px solid blue");
+        exampleForm.setBorder("3px solid #0083ff");
         
         TextItem nameField = new TextItem("Name");
         
