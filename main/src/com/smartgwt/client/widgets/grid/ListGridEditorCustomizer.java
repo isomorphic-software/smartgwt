a589d3cbfa24870fc34f4702c3fb862c25161036
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-07-06 19:16:18 +0000

    Reworking how ListGridEditorCustomizer behaves - returning the result of 'getDefaultProperties' is no longer required to pick up default behavior -- any custom props are simply applied on top of defaults, and you can return null for totally standard behavior

--- main/src/com/smartgwt/client/widgets/grid/ListGridEditorCustomizer.java
+++ main/src/com/smartgwt/client/widgets/grid/ListGridEditorCustomizer.java
@@ -13,7 +13,10 @@ public interface ListGridEditorCustomizer {
      * a template (properties block) for the item displayed to the user - as with {@link ListGridField#setEditorType()} 
      * if applying event handlers to the item, the live item should be retrieved from the event object.
      * <P>
-     * For default behavior simply return the result of {@link ListGridEditorContext#getDefaultProperties()}.    
+     * Any properties specified on the returned form item will be applied on top of the default form item properties
+     * derived from {@link ListGridField#getType()}, {@link ListGridField#setEditorType(FormItem)} etc. For default
+     * behavior, simply return null.
+     * 
      * @param context context object containing details of the cell being edited.
      * @return template FormItem.
      */
