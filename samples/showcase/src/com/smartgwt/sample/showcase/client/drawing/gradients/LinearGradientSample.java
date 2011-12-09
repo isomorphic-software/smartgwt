f2aa334ee71fe8e4bd18ee0826678703f2e344ae
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2011-12-09 12:14:48 +0000

    Merging to trunk: LGPL Showcase: - The Linear Gradient sample was actually a Radial Gradient sample, and a fairly broken one at that.  Renamed it to Radial Gradient and fixed the bugs. - The Radial Gradient sample was just a copy of the Simple Gradient sample, with some of the variables renamed radial-Something rather than simple-Something, and a completely bogus description applied.  Replaced all this with a port of the SmartClient Linear Gradient sample. - All three of the gradient samples have very short and unhelpful descriptions, but the equivalent SmartClient samples are the same, so I left them alone. - The Drawing Mouse Events sample apparently did nothing at all.  Actually, it was logging mouse events to the SmartClient log, though there was nothing to indicate that this was the case.  Changed it to show the mouse events in an on-screen label instead - don't know if this is correct, but there is no corresponding SmartClient sample for me to copy.

--- samples/showcase/src/com/smartgwt/sample/showcase/client/drawing/gradients/LinearGradientSample.java
+++ samples/showcase/src/com/smartgwt/sample/showcase/client/drawing/gradients/LinearGradientSample.java
@@ -9,8 +9,8 @@ import com.smartgwt.client.widgets.drawing.DrawOval;
 import com.smartgwt.client.widgets.drawing.DrawPane;
 import com.smartgwt.client.widgets.drawing.DrawRect;
 import com.smartgwt.client.widgets.drawing.DrawTriangle;
+import com.smartgwt.client.widgets.drawing.LinearGradient;
 import com.smartgwt.client.widgets.drawing.Point;
-import com.smartgwt.client.widgets.drawing.RadialGradient;
 import com.smartgwt.client.widgets.events.DrawEvent;
 import com.smartgwt.client.widgets.events.DrawHandler;
 import com.smartgwt.client.widgets.form.DynamicForm;
@@ -18,15 +18,16 @@ import com.smartgwt.client.widgets.form.events.ItemChangedEvent;
 import com.smartgwt.client.widgets.form.events.ItemChangedHandler;
 import com.smartgwt.client.widgets.form.fields.ColorPickerItem;
 import com.smartgwt.client.widgets.form.fields.SliderItem;
-import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.VStack;
 import com.smartgwt.sample.showcase.client.PanelFactory;
 import com.smartgwt.sample.showcase.client.ShowcasePanel;
 
 public class LinearGradientSample extends ShowcasePanel {
-    private static final String DESCRIPTION = "Sample of using radial type of gradient.";
+    private static final String DESCRIPTION = "Sample of using linear type of gradient.";
     private DrawPane drawPane;
-    private DynamicForm radialGradientDynamicForm;
-    private DynamicForm radielGradientSliderDynamicForm;
+    private DynamicForm linearGradientDynamicForm,
+                        linearGradientSliderDynamicForm;
 
     public static class Factory implements PanelFactory {
 
@@ -49,17 +50,17 @@ public class LinearGradientSample extends ShowcasePanel {
 
     public Canvas getViewPanel() {
 
+        /* ------------------------------------------- */
+        
         drawPane = new DrawPane();
-        drawPane.setTop(1850);
+        drawPane.setTop(950);
         drawPane.setHeight(400);
         drawPane.setLeft(25);
         drawPane.setWidth(400);
-        drawPane.setShowEdges(true);
         drawPane.setEdgeSize(4);
         drawPane.setBackgroundColor("papayawhip");
         drawPane.setOverflow(Overflow.HIDDEN);
         drawPane.setCursor(Cursor.AUTO);
-        drawPane.draw();
 
         drawPane.addDrawHandler(new DrawHandler() {
             
@@ -68,13 +69,20 @@ public class LinearGradientSample extends ShowcasePanel {
 
                 DrawPane drawPane = (DrawPane) event.getSource();
 
-                radialGradientShapesDraw(drawPane);
+                linearGradientShapesDraw(drawPane);
             }
             
         });
         
+        ItemChangedHandler linearGradientItemChangedHandler = new ItemChangedHandler() {
+            @Override
+            public void onItemChanged(ItemChangedEvent event) {
+                linearGradientShapesDraw(drawPane);
+            }
+
+        };
         ColorPickerItem startColorColorPicker = new ColorPickerItem("startColor","Start Color");
-        ColorPickerItem firstStopColorPicker  = new ColorPickerItem("firstStop","First Stop Color");
+        ColorPickerItem firstStopColorPicker = new ColorPickerItem("firstStop","First Stop Color");
         ColorPickerItem secondStopColorPicker = new ColorPickerItem("secondStop","Second Stop Color");
         ColorPickerItem endColorColorPicker   = new ColorPickerItem("endColor","End Color");
 
@@ -83,86 +91,100 @@ public class LinearGradientSample extends ShowcasePanel {
         secondStopColorPicker.setDefaultValue("#00ff00");
         endColorColorPicker.setDefaultValue("#0000ff");
 
-        ItemChangedHandler radialGradientItemChangedHandler = new ItemChangedHandler() {
-            @Override
-            public void onItemChanged(ItemChangedEvent event) {
-                radialGradientShapesDraw(drawPane);
-            }
+        linearGradientDynamicForm = new DynamicForm();
+        linearGradientDynamicForm.setID("linearGradientDynamicForm");
+        linearGradientDynamicForm.setWidth(250);
+        linearGradientDynamicForm.addItemChangedHandler(linearGradientItemChangedHandler);
+        linearGradientDynamicForm.setFields(startColorColorPicker, 
+                                            firstStopColorPicker, 
+                                            secondStopColorPicker, 
+                                            endColorColorPicker);
 
+        SliderItem x1Slider = new SliderItem("x1");
+        x1Slider.setMinValue(1);
+        x1Slider.setMaxValue(100);
+        x1Slider.setHeight(20);
+        x1Slider.setDefaultValue(20);
 
-        };
+        SliderItem y1Slider = new SliderItem("y1");
+        y1Slider.setMinValue(1);
+        y1Slider.setMaxValue(100);
+        y1Slider.setHeight(20);
+        y1Slider.setDefaultValue(20);
+
+        SliderItem x2Slider = new SliderItem("x2");
+        x2Slider.setMinValue(1);
+        x2Slider.setMaxValue(100);
+        x2Slider.setHeight(20);
+        x2Slider.setDefaultValue(80);
+
+        SliderItem y2Slider = new SliderItem("y2");
+        y2Slider.setMinValue(1);
+        y2Slider.setMaxValue(100);
+        y2Slider.setHeight(20);
+        y2Slider.setDefaultValue(80);
         
-        radialGradientDynamicForm = new DynamicForm();
-        radialGradientDynamicForm.setTop(1850);
-        radialGradientDynamicForm.setWidth(270);
-        radialGradientDynamicForm.setLeft(475);
-        radialGradientDynamicForm.setFields(startColorColorPicker,firstStopColorPicker,secondStopColorPicker,endColorColorPicker);
-        radialGradientDynamicForm.addItemChangedHandler(radialGradientItemChangedHandler);
-        radialGradientDynamicForm.draw();
+        linearGradientSliderDynamicForm = new DynamicForm();
+        linearGradientSliderDynamicForm.setWidth(270);
+        linearGradientSliderDynamicForm.setFields(x1Slider, y1Slider, x2Slider, y2Slider);
+        linearGradientSliderDynamicForm.addItemChangedHandler(linearGradientItemChangedHandler);
+        linearGradientSliderDynamicForm.draw();
+        
+        linearGradientShapesDraw(drawPane);
         
 
-        SliderItem rSlider = new SliderItem("r");
-        rSlider.setDefaultValue(100);
-        rSlider.setMinValue(0);
-        rSlider.setMaxValue(100);
-        rSlider.setHeight(20);
-        
-        radielGradientSliderDynamicForm = new DynamicForm();
-        radielGradientSliderDynamicForm.setTop(1950);
-        radielGradientSliderDynamicForm.setWidth(270);
-        radielGradientSliderDynamicForm.setLeft(475);
-        radielGradientSliderDynamicForm.setFields(rSlider);
-        radielGradientSliderDynamicForm.addItemChangedHandler(radialGradientItemChangedHandler);
-        radielGradientSliderDynamicForm.draw();
-
-
-        VLayout layout = new VLayout();
-        layout.addMember(drawPane);
-        layout.addMember(radialGradientDynamicForm);
-        layout.addMember(radielGradientSliderDynamicForm);
+        VStack layout = new VStack();
+        HLayout layout2 = new HLayout();
+        layout2.addMember(drawPane);
+        layout2.addMember(linearGradientSliderDynamicForm);
+        layout.addMember(layout2);
+        layout.addMember(linearGradientDynamicForm);
+        layout.setMembersMargin(15);
         layout.draw();
         
         return layout;
     }
+    
+    /**
+     * This method is called to re-render the pane that contains the shapes with
+     *  a defined gradient in them.
+     */
+    private void linearGradientShapesDraw(DrawPane drawPane) {
 
-    private void radialGradientShapesDraw(DrawPane drawPane) {
         drawPane.erase();
 
         ColorStop colorStop1 = new ColorStop();
-        colorStop1.setColor(radialGradientDynamicForm.getValueAsString("startColor"));
+        colorStop1.setColor(linearGradientDynamicForm.getValueAsString("startColor"));
         colorStop1.setOffset(0.0f);
 
         ColorStop colorStop2 = new ColorStop();
-        colorStop2.setColor(radialGradientDynamicForm.getValueAsString("firstStop"));
+        colorStop2.setColor(linearGradientDynamicForm.getValueAsString("firstStop"));
         colorStop2.setOffset(0.33f);
 
         ColorStop colorStop3 = new ColorStop();
-        colorStop3.setColor(radialGradientDynamicForm.getValueAsString("secondStop"));
+        colorStop3.setColor(linearGradientDynamicForm.getValueAsString("secondStop"));
         colorStop3.setOffset(0.66f);
 
         ColorStop colorStop4 = new ColorStop();
-        colorStop4.setColor(radialGradientDynamicForm.getValueAsString("endColor"));
+        colorStop4.setColor(linearGradientDynamicForm.getValueAsString("endColor"));
         colorStop4.setOffset(1.0f);
         
-        int r = (Integer) radielGradientSliderDynamicForm.getValue("r");
+        LinearGradient linearGradient = new LinearGradient();
+        linearGradient.setX1(linearGradientSliderDynamicForm.getValue("x1") + "%");
+        linearGradient.setY1(linearGradientSliderDynamicForm.getValue("y1") + "%");
+        linearGradient.setX2(linearGradientSliderDynamicForm.getValue("x2") + "%");
+        linearGradient.setY2(linearGradientSliderDynamicForm.getValue("y2") + "%");
+        linearGradient.setColorStops(colorStop1, colorStop2, colorStop3, colorStop4);
         
-        RadialGradient radialGradient = new RadialGradient();
-        radialGradient.setCx("0%");
-        radialGradient.setCy("0%");
-        radialGradient.setR(r+"%");
-        radialGradient.setFx("0%");
-        radialGradient.setFy("0%");
-        radialGradient.setColorStops(colorStop1,colorStop2,colorStop3,colorStop4);
-               
         DrawTriangle drawTriangle = new DrawTriangle();
         drawTriangle.setDrawPane(drawPane);
-        drawTriangle.setFillGradient(radialGradient);
-        drawTriangle.setPoints(new Point(100,50),new Point(150,150),new Point(50,150));
+        drawTriangle.setFillGradient(linearGradient);
+        drawTriangle.setPoints(new Point(100,50), new Point(150,150), new Point(50,150));
         drawTriangle.draw();
         
         DrawCurve drawCurve = new DrawCurve();
         drawCurve.setDrawPane(drawPane);
-        drawCurve.setFillGradient(radialGradient);
+        drawCurve.setFillGradient(linearGradient);
         drawCurve.setStartPoint(new Point(200,50));
         drawCurve.setEndPoint(new Point(340,150));
         drawCurve.setControlPoint1(new Point(270,0));
@@ -171,7 +193,7 @@ public class LinearGradientSample extends ShowcasePanel {
         
         DrawOval drawOval = new DrawOval();
         drawOval.setDrawPane(drawPane);
-        drawOval.setFillGradient(radialGradient);
+        drawOval.setFillGradient(linearGradient);
         drawOval.setLeft(50);
         drawOval.setTop(200);
         drawOval.setWidth(100);
@@ -180,13 +202,13 @@ public class LinearGradientSample extends ShowcasePanel {
         
         DrawRect drawRect = new DrawRect();
         drawRect.setDrawPane(drawPane);
-        drawRect.setFillGradient(radialGradient);
+        drawRect.setFillGradient(linearGradient);
         drawRect.setLeft(200);
         drawRect.setTop(225);
         drawRect.setWidth(150);
         drawRect.setHeight(100);
         drawRect.draw();
-        
+               
     }
 
     public String getIntro() {
