b1b7d4dc5bd62b392b24c2abff4e14dfff336d74
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-03-20 09:11:22 +0000

    The java 1.6 does not support a code '(TYPE[]) new Object[values.length];'

--- trunk/main/src/com/smartgwt/client/util/EnumUtil.java
+++ trunk/main/src/com/smartgwt/client/util/EnumUtil.java
@@ -2,11 +2,28 @@ package com.smartgwt.client.util;
 
 import com.smartgwt.client.types.ValueEnum;
 
+/**
+ * Utils for manipulate with a Enum.
+ */
 public class EnumUtil {
 
-    public static <T extends ValueEnum> T getEnum(T[] enums, String value) {
-        if(value == null) return null;
-        for (T anEnum : enums) {
+    /**
+     * Get a enum item by value.
+     * 
+     * @param enums
+     *            - enum items
+     * @param value
+     *            - enum value
+     * @return enum item
+     * 
+     * @see com.smartgwt.client.types.ValueEnum
+     */
+    public static <TYPE extends ValueEnum> TYPE getEnum(TYPE[] enums, String value) {
+        if (value == null) {
+            return null;
+        }
+
+        for (TYPE anEnum : enums) {
             if (anEnum.getValue().equals(value)) {
                 return anEnum;
             }
@@ -14,18 +31,71 @@ public class EnumUtil {
         return null;
     }
 
-    public static <T extends ValueEnum> ValueEnum[] getEnums(T[] enums, String[] values) {
-        if(values == null) return null;
-        ValueEnum ret[] = new ValueEnum[values.length];
-        for (int i=0; i<values.length; i++) {
+    /**
+     * Get a enum items by values.
+     * <p>
+     * The java 1.6 does not support a code "
+     * {@code (TYPE[]) new Object[values.length];". Also, the GWT does not support a reflection api (
+     * {@link java.lang.reflect.Array#newInstance}). So you need to pass an
+     * array argument to store the result.
+     * 
+     * @param enums
+     *            - enum items
+     * @param values
+     *            - enum values
+     * @param result
+     *            - array to store the result
+     * @return enum items
+     */
+    public static <TYPE extends ValueEnum> TYPE[] getEnums(TYPE[] enums, String[] values, TYPE[] result) {
+        if (values == null) {
+            return result;
+        }
+
+        for (int i = 0; i < values.length; i++) {
             String value = values[i];
-            ret[i] = null;
-            for (T anEnum : enums) {
+            result[i] = null;
+            for (TYPE anEnum : enums) {
                 if (anEnum.getValue().equals(value)) {
-                    ret[i] = (ValueEnum) anEnum;
+                    result[i] = anEnum;
+                    break;
                 }
             }
         }
-        return ret;
+        return result;
+    }
+
+    /**
+     * Get a enum items by values.
+     * <p>
+     * This method is not compatible with the java 1.6.
+     * 
+     * @param enums
+     *            - enum items
+     * @param values
+     *            - enum values
+     * @return enum items
+
+     * @deprecated use {{@link #getEnums(ValueEnum[], String[], ValueEnum[])}
+     *             method
+     * @see EnumUtil#getEnums(ValueEnum[], String[], ValueEnum[])             
+     */
+    public static <TYPE extends ValueEnum> ValueEnum[] getEnums(TYPE[] enums, String[] values) {
+        if (values == null) {
+            return null;
+        }
+
+        ValueEnum result[] = new ValueEnum[values.length];
+        for (int i = 0; i < values.length; i++) {
+            String value = values[i];
+            result[i] = null;
+            for (TYPE anEnum : enums) {
+                if (anEnum.getValue().equals(value)) {
+                    result[i] = (ValueEnum) anEnum;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
