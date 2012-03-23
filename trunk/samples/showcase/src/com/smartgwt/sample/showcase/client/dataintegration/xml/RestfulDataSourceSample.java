bd09f2b511e60a2a36da903c399892657a0a5c14
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-03-23 23:03:10 +0000

    Comments were added before the code that creates operationBindings.

--- trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/dataintegration/xml/RestfulDataSourceSample.java
+++ trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/dataintegration/xml/RestfulDataSourceSample.java
@@ -65,6 +65,9 @@ public class RestfulDataSourceSample extends ShowcasePanel {
                 super.transformResponse(response, request, data);
             }
         };
+        // These lines are not required for this sample to work, but they demonstrate how you can configure RestDataSource 
+        // with OperationBindings in order to control settings such as whether to use the GET, POST or PUT HTTP methods,
+        // and whether to send data as URL parameters vs as posted JSON or XML messages.
         DataSourceTextField countryCode = new DataSourceTextField("countryCode", "Code");
         OperationBinding fetch = new OperationBinding();
         fetch.setOperationType(DSOperationType.FETCH);
