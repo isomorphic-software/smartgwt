ca3963a54b6f786cdee45b8282610878931a8c38
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-05-08 22:50:29 +0000

    Add to the JavaDoc for RPCCallback.execute()

--- main/src/com/smartgwt/client/rpc/RPCCallback.java
+++ main/src/com/smartgwt/client/rpc/RPCCallback.java
@@ -18,10 +18,14 @@ package com.smartgwt.client.rpc;
 public interface RPCCallback {
 
     /**
+     * Executes when an {@link RPCRequest} completes.
      *
      * @param response a RPCResponse encapsulating the server response to your request
-     * @param rawData just the "data" property from the RPCResponse, for convenience
-     * @param request the RPCRequest that was sent. You can use RPCRequest.clientContext to track state during the server turnaround
+     * @param rawData The "data" property from the RPCResponse, for convenience.  The data can
+     * also be obtained via {@link RPCResponse#getDataAsMap()}, {@link RPCResponse#getDataAsString()},
+     * or {@link RPCResponse#getDataAsObject()}, depending on the type of data that is expected to be
+     * returned from the server.
+     * @param request the RPCRequest that was sent.
      */
     void execute(RPCResponse response, Object rawData, RPCRequest request);
 
