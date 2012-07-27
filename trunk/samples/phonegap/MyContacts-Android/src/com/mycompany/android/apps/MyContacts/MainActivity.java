5fe78652169d7b1fc0c27d31be55de1a3d2d0e3f
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-07-27 14:07:06 +0000

    Committed the MyContacts Android project.

---
+++ trunk/samples/phonegap/MyContacts-Android/src/com/mycompany/android/apps/MyContacts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mycompany.android.apps.MyContacts;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
