b45ecee709c1b0586129e640b46fa399b1361dc0
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-10-27 00:58:03 +0000

    New loadScreenCallback interface for use with RPCManager.loadScreen API

---
+++ main/src/com/smartgwt/client/rpc/LoadScreenCallback.java
@@ -0,0 +1,14 @@
+package com.smartgwt.client.rpc;
+
+/**
+ * RPCManager load screen callback
+ * @see com.smartgwt.client.rpc.RPCManager#loadScreen(String, LoadScreenCallback)
+ *
+ */
+public interface LoadScreenCallback {
+
+    /**
+     * RPCManager load screen callback
+     */
+    void execute();
+}
