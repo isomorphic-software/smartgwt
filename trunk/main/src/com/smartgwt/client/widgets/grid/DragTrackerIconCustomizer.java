58c95f208c8bb55da9d4a8915200cd7374bed7a0
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2012-12-07 06:17:41 +0000

    Missing class file.

---
+++ trunk/main/src/com/smartgwt/client/widgets/grid/DragTrackerIconCustomizer.java
@@ -0,0 +1,34 @@
+package com.smartgwt.client.widgets.grid;
+
+import com.smartgwt.client.data.Record;
+
+/**
+ * Hover customizer object for returning custom HTML to display in a hover prompt for ListGrid cells
+ */
+public abstract class DragTrackerIconCustomizer {
+    
+    private ListGrid grid;
+
+    public void setGrid(ListGrid grid) {
+        this.grid = grid;
+    }    
+
+    /**
+     * Returns the ListGrid over which the hover occurred
+     * @return ListGrid over which the hover occurred
+     */
+    public ListGrid getGrid() {
+        return this.grid;
+    }
+    
+    /**
+     * hoverHTML() should be implemented and return the string of HTML to display when the user hovers
+     * over ListGrid cells
+     * @param value
+     * @param record
+     * @param rowNum
+     * @param colNum
+     * @return HTML to display
+     */
+    public abstract String setDragTrackerIcon(Record[] records);
+}
