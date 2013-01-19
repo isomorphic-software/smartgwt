4b12b56cfb42fed1173ca7e310682d514c708c08
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-01-19 00:30:29 +0000

    Add new sample to illustrate grouping a ListGrid with multiple fields.

--- trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/data/OrderItemData.java
+++ trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/data/OrderItemData.java
@@ -30,26 +30,30 @@ public class OrderItemData {
 
     public static OrderItem[] getNewRecords() {
         return new OrderItem[]{
-                new OrderItem(0, 8805, "Anti Virus Suite", "Software", new Date(109, 10,20), 2, 50),
-                new OrderItem(1, 4833, "USB Ergonomic Keyboard", "Hardware", new Date(109, 10,13), 1, 99.99f),
-                new OrderItem(2, 9551, "Lumbar Support Cushion", "Hardware", new Date(109, 1, 2), 1, 75),
-                new OrderItem(3, 954, "USB Keyboard", "Hardware", new Date(109, 10,20), 12, 34.99f),
+                new OrderItem(0, 8805, "Anti Virus Suite", "Software", new Date(110, 2, 5), 2, 50),
+                new OrderItem(1, 4833, "USB Ergonomic Keyboard", "Hardware", new Date(109, 10, 13), 1, 99.99f),
+                new OrderItem(2, 9551, "Lumbar Support Cushion", "Hardware", new Date(110, 1, 2), 1, 75),
+                new OrderItem(3, 954, "USB Keyboard", "Hardware", new Date(109, 10, 20), 12, 34.99f),
+
                 new OrderItem(4, 616, "Consulting Services", "Services", new Date(109, 10, 10), 40, 100),
-                new OrderItem(5, 8357, "Office Application Suite", "Software", new Date(109, 2,5), 14, 30),
-                new OrderItem(6, 4582, "Anti Virus Suite", "Software", new Date(109, 1,20), 22, 50),
-                new OrderItem(7, 9373, "On-site Training", "Services", new Date(109, 10,20), 20, 100),
-                new OrderItem(8, 9311, "Software Development Kit", "Software", new Date(109, 10,10), 8, 249.99f),
-                new OrderItem(9, 7806, "15 inch flat panel monitor", "Hardware", new Date(109, 11,12), 4, 300),
-                new OrderItem(10, 8620, "18 inch flat panel monitor", "Hardware", new Date(109, 10,20), 2, 50),
-                new OrderItem(11, 629, "Office Productivity Suite", "Software", new Date(109, 11,11), 2, 64.99f),
-                new OrderItem(12, 1264, "Office Productivity Suite", "Software", new Date(109, 12,20), 2, 64.99f),
-                new OrderItem(13, 7100, "USB Ergonomic Keyboard", "Hardware", new Date(109, 10,20), 6, 99.99f),
-                new OrderItem(14, 9647, "Consulting Services", "Services", new Date(109, 10,21), 10, 100),
-                new OrderItem(15, 8468, "USB Optical Mouse", "Hardware", new Date(109, 10,10), 5, 29.99f),
-                new OrderItem(16, 9496, "Off-site Training", "Services", new Date(109, 1,1), 35, 80),
-                new OrderItem(17, 5124, "USB Stereo Headset", "Hardware", new Date(109, 10,20), 1, 69.99f),
-                new OrderItem(18, 1799, "Office Application Suite", "Software", new Date(109, 11,2), 222, 30),
-                new OrderItem(19, 7020, "Consulting Services", "Services", new Date(109, 10,20), 100, 80)
+                new OrderItem(5, 8357, "Office Application Suite", "Software", new Date(110, 2, 5), 14, 30),
+                new OrderItem(6, 4582, "Anti Virus Suite", "Software", new Date(110, 1, 20), 22, 50),
+                new OrderItem(7, 9373, "On-site Training", "Services", new Date(109, 10, 10), 20, 100),
+
+                new OrderItem(8, 9311, "Software Development Kit", "Software", new Date(110, 1, 20), 8, 249.99f),
+                new OrderItem(9, 7806, "15 inch flat panel monitor", "Hardware", new Date(109, 10, 13), 4, 300),
+                new OrderItem(10, 8620, "18 inch flat panel monitor", "Hardware", new Date(110, 1, 2), 2, 50),
+
+                new OrderItem(11, 629, "Office Productivity Suite", "Software", new Date(110, 2, 5), 2, 64.99f),
+                new OrderItem(12, 1264, "Office Productivity Suite", "Software", new Date(110, 2, 5), 2, 64.99f),
+                new OrderItem(13, 7100, "USB Ergonomic Keyboard", "Hardware", new Date(109, 10, 20), 6, 99.99f),
+                new OrderItem(14, 9647, "Consulting Services", "Services", new Date(109, 10,10), 10, 100),
+
+                new OrderItem(15, 8468, "USB Optical Mouse", "Hardware", new Date(109, 10, 13), 5, 29.99f),
+                new OrderItem(16, 9496, "Off-site Training", "Services", new Date(109, 10, 10), 35, 80),
+                new OrderItem(17, 5124, "USB Stereo Headset", "Hardware", new Date(109, 10, 20), 1, 69.99f),
+                new OrderItem(18, 1799, "Office Application Suite", "Software", new Date(110, 1, 20), 222, 30),
+                new OrderItem(19, 7020, "Consulting Services", "Services", new Date(109, 10, 10), 100, 80)
         };
     }
 }
