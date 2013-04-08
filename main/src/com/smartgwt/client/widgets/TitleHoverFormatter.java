660a07d3034df34c40378f88c0ed3365c2dfa746
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-04-08 14:03:16 +0000

    Added the defaultHTML parameter to TitleHoverFormatter.getHoverHTML() and HeaderHoverFormatter.getHoverHTML().

--- main/src/com/smartgwt/client/widgets/TitleHoverFormatter.java
+++ main/src/com/smartgwt/client/widgets/TitleHoverFormatter.java
@@ -3,10 +3,13 @@ package com.smartgwt.client.widgets;
 public interface TitleHoverFormatter {
 
     /**
-     * Returns the HTML to display in the standard hover when the user holds the mouse pointer over
-     * the component and the title is clipped. Return null to suppress the hover canvas altogether.
+     * Returns the HTML that is displayed by default if a registered
+     * {@link com.smartgwt.client.widgets.events.TitleHoverHandler} does not cancel a
+     * {@link com.smartgwt.client.widgets.events.TitleHoverEvent}. Return null or an empty string
+     * to cancel the hover.
      *
-     * @return HTML to be displayed in the hover
+     * @param defaultHTML the HTML that would have been displayed by default.
+     * @return HTML to be displayed in the hover. If null or an empty string, then the hover is canceled.
      */
-    public String getHoverHTML();
+    public String getHoverHTML(String defaultHTML);
 }
