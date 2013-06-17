0d141f83387675c976857c0b538a56dd8bb13379
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-06-17 15:54:38 +0000

    Generalized the com.smartgwt.client.widgets.rte package comment.
    [dt]

--- main/src/com/smartgwt/client/widgets/rte/package-info.java
+++ main/src/com/smartgwt/client/widgets/rte/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package contains helper classes for the {@link com.smartgwt.client.widgets.RichTextEditor}'s
- * HTML list editing support.
+ * This package contains helper classes for the {@link com.smartgwt.client.widgets.RichTextEditor}
+ * (e.g. the helper widgets for its HTML list editing support).
  */
 package com.smartgwt.client.widgets.rte;
