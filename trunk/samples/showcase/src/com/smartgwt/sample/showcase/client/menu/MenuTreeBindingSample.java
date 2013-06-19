16348f2f3c5510278e6fab7830eeb8ce44186b07
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-06-19 18:30:45 +0000

    Fixed a small bug that made the first menu to fail popping up an alert when item was selected -This line, and those below, will be ignored--
    M    MenuTreeBindingSample.java

--- trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/menu/MenuTreeBindingSample.java
+++ trunk/samples/showcase/src/com/smartgwt/sample/showcase/client/menu/MenuTreeBindingSample.java
@@ -4,15 +4,16 @@ import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.menu.IMenuButton;
 import com.smartgwt.client.widgets.menu.Menu;
-import com.smartgwt.client.widgets.menu.MenuButton;
 import com.smartgwt.client.widgets.menu.MenuItem;
 import com.smartgwt.client.widgets.menu.events.ItemClickEvent;
 import com.smartgwt.client.widgets.menu.events.ItemClickHandler;
+import com.smartgwt.client.widgets.tree.Tree;
+import com.smartgwt.client.widgets.tree.TreeNode;
 import com.smartgwt.sample.showcase.client.PanelFactory;
 import com.smartgwt.sample.showcase.client.ShowcasePanel;
 import com.smartgwt.sample.showcase.client.data.SupplyCategoryXmlDS;
 
-public class MenuTreeBindingSample extends ShowcasePanel {
+public class MenuTreeBindingSample extends ShowcasePanel {	
 	private static final String DESCRIPTION = "Click on \"Department\" or \"Category\" below to show hierarchical menus." +
             " The \"Category\" menu loads options dynamically from the SupplyCategory DataSource.";
 
@@ -40,12 +41,12 @@ public class MenuTreeBindingSample extends ShowcasePanel {
 
 		Menu mDepartment = new Menu();
 		mDepartment.setCanSelectParentItems(true);
-		mDepartment.setData(getLocalItemsAsArray());
+		mDepartment.setData(getLocalItemsAsTree());
 		mDepartment.setWidth(130);
 
 		mDepartment.addItemClickHandler(new ItemClickHandler() {
 			public void onItemClick(ItemClickEvent event) {
-				SC.say("You picked the \"" + event.getItem().getTitle()
+				SC.say("You picked the \"" + event.getTreeNode().getName()
 						+ "\" department.");
 			}
 		});
@@ -78,31 +79,34 @@ public class MenuTreeBindingSample extends ShowcasePanel {
 		return main;
 	}
 
-	private MenuItem[] getLocalItemsAsArray() {
+	private Tree getLocalItemsAsTree() {
+		Tree retVal = new Tree();
+		
+		TreeNode[] treeNodes = new TreeNode[4];
+		
+		treeNodes[0] = new TreeNode("Marketing");
+		Tree menuMarketing = new Tree();
+		menuMarketing.setData(new TreeNode[]{new TreeNode("Advertising"), new TreeNode("Community Relations")});
+		treeNodes[0].setChildren(menuMarketing.getAllNodes());
+		
+		treeNodes[1] = new TreeNode("Sales");
+		Tree menuSales = new Tree();
+		menuSales.setData(new TreeNode[]{new TreeNode("Direct Sales"), new TreeNode("Channel Sales")});
+		treeNodes[1].setChildren(menuSales.getAllNodes());
+		
+		treeNodes[2] = new TreeNode("Manufacturing");
+		Tree menuManufacturing = new Tree();
+		menuManufacturing.setData(new TreeNode[]{new TreeNode("Design"), new TreeNode("Development"), new TreeNode("QA")});
+		treeNodes[2].setChildren(menuManufacturing.getAllNodes());
 
-		MenuItem[] m = new MenuItem[4];
-
-		m[0] = new MenuItem("Marketing");
-		Menu menuMarketing = new Menu();
-		menuMarketing.setData(new MenuItem("Advertising"), new MenuItem("Community Relations"));
-		m[0].setSubmenu(menuMarketing);
-
-		m[1] = new MenuItem("Sales");
-		Menu menuSales = new Menu();
-		menuSales.setData(new MenuItem("Direct Sales"),new MenuItem("Channel Sales"));
-		m[1].setSubmenu(menuSales);
-
-		m[2] = new MenuItem("Manufacturing");
-		Menu menuManufacturing = new Menu();
-		menuManufacturing.setData(new MenuItem("Design"), new MenuItem("Development"), new MenuItem("QA"));
-		m[2].setSubmenu(menuManufacturing);
-
-		m[3] = new MenuItem("Services");
-		Menu menuServices = new Menu();
-		menuServices.setData(new MenuItem("Support"), new MenuItem("Consulting"));
-		m[3].setSubmenu(menuServices);
-
-		return m;
+		treeNodes[3] = new TreeNode("Services");
+		Tree menuServices = new Tree();
+		menuServices.setData(new TreeNode[]{new TreeNode("Support"), new TreeNode("Consulting")});
+		treeNodes[3].setChildren(menuServices.getAllNodes());
+		
+		retVal.setData(treeNodes);
+		
+		return retVal;
 	}
 
 	public String getIntro() {
