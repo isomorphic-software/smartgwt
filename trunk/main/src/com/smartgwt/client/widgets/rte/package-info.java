93c70cee6cd7f908881f2f9e809802bcaba36f35
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-08-14 16:52:58 +0000

    Fixed the com.smartgwt.client.widgets.rte package comment so that it does not trail off after "e.g.".
    [dt]

--- trunk/main/src/com/smartgwt/client/widgets/rte/package-info.java
+++ trunk/main/src/com/smartgwt/client/widgets/rte/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package contains helper classes for the {@link com.smartgwt.client.widgets.RichTextEditor}
- * (e.g. the helper widgets for its HTML list editing support).
+ * (e&#46;g&#46; the helper widgets for its HTML list editing support).
  */
 package com.smartgwt.client.widgets.rte;
