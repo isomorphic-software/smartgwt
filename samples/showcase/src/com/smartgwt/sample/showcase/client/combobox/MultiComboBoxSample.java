2ac9c7e879a3b6f0f7d60a6a9dde560ed95b1f96
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-08-18 13:41:19 +0000

    Ported improvements to the Multi ComboBox Feature Explorer sample to SGWT Showcase.
    [dt]

--- samples/showcase/src/com/smartgwt/sample/showcase/client/combobox/MultiComboBoxSample.java
+++ samples/showcase/src/com/smartgwt/sample/showcase/client/combobox/MultiComboBoxSample.java
@@ -9,25 +9,22 @@ import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.MultiComboBoxItem;
 import com.smartgwt.client.widgets.form.fields.SelectItem;
-import com.smartgwt.client.widgets.form.fields.SpacerItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
 import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
+import com.smartgwt.client.widgets.layout.VStack;
 import com.smartgwt.sample.showcase.client.PanelFactory;
 import com.smartgwt.sample.showcase.client.ShowcasePanel;
 import com.smartgwt.sample.showcase.client.data.ItemSupplyXmlDS;
 
 public class MultiComboBoxSample extends ShowcasePanel {
-    private static final String DESCRIPTION = "An example of a MultiComboBox";
-
-    private static final Map<String, MultiComboBoxLayoutStyle> layoutStyles;
-    static {
-        final MultiComboBoxLayoutStyle[] values = MultiComboBoxLayoutStyle.values();
-        layoutStyles = new HashMap<String, MultiComboBoxLayoutStyle>(values.length);
-
-        for (MultiComboBoxLayoutStyle value : values) {
-            layoutStyles.put(value.getValue(), value);
-        }
-    }
+    private static final String DESCRIPTION = "A <code>MultiComboBoxItem</code> displays a selection of multiple values as " + 
+            "buttons along with a combo box that is used to select additional values. " + 
+            "Clicking on a button removes the value from the selection." + 
+            "<p>" + 
+            "Pressing the Enter/Return key within the combo box selects the first matching value " + 
+            "without leaving the field, allowing several values to be selected using only the " + 
+            "keyboard. Pressing the Tab key within the combo box selects the first matching value " + 
+            "and then leaves the field normally, taking focus to the next focusable item.";
 
     public static class Factory implements PanelFactory {
         private String id;
@@ -47,42 +44,58 @@ public class MultiComboBoxSample extends ShowcasePanel {
         }
     }
 
+    private static final Map<String, MultiComboBoxLayoutStyle> LAYOUT_STYLES;
+    static {
+        final MultiComboBoxLayoutStyle[] values = MultiComboBoxLayoutStyle.values();
+        LAYOUT_STYLES = new HashMap<String, MultiComboBoxLayoutStyle>(values.length);
+
+        for (MultiComboBoxLayoutStyle value : values) {
+            LAYOUT_STYLES.put(value.getValue(), value);
+        }
+    }
+
+    @Override
     public Canvas getViewPanel() {
 
-        final MultiComboBoxLayoutStyle initialLayoutStyle = MultiComboBoxLayoutStyle.VERTICAL;
+        final MultiComboBoxLayoutStyle initialLayoutStyle = MultiComboBoxLayoutStyle.FLOW;
 
-        final DynamicForm form = new DynamicForm();
-        form.setMargin(10);
-        form.setNumCols(2);
-        form.setColWidths(500, 120);
-        form.setCellPadding(15);
-        form.setTitleOrientation(TitleOrientation.TOP);
-
-        final MultiComboBoxItem supplies = new MultiComboBoxItem("supplies", "Items");
-        supplies.setOptionDataSource(ItemSupplyXmlDS.getInstance());
-        supplies.setDisplayField("itemName");
-        supplies.setValueField("itemID");
-        supplies.setAutoFetchData(true);
-        supplies.setLayoutStyle(initialLayoutStyle);
-
-        final SpacerItem spacer1 = new SpacerItem(), spacer2 = new SpacerItem();
+        final MultiComboBoxItem suppliesItem = new MultiComboBoxItem("supplies", "Items");
+        suppliesItem.setOptionDataSource(ItemSupplyXmlDS.getInstance());
+        suppliesItem.setDisplayField("itemName");
+        suppliesItem.setValueField("itemID");
+        suppliesItem.setValue(new int[] { 1, 10, 23, 123 });
+        suppliesItem.setAutoFetchData(true);
+        suppliesItem.setLayoutStyle(initialLayoutStyle);
 
         final SelectItem layoutStyleSelector = new SelectItem();
         layoutStyleSelector.setTitle("Change layout style");
         layoutStyleSelector.setDefaultValue(initialLayoutStyle.getValue());
-        layoutStyleSelector.setValueMap(layoutStyles.keySet().toArray(new String[0]));
+        layoutStyleSelector.setValueMap(LAYOUT_STYLES.keySet().toArray(new String[LAYOUT_STYLES.size()]));
         layoutStyleSelector.addChangedHandler(new ChangedHandler() {
             @Override
             public void onChanged(ChangedEvent event) {
-                String value = (String) event.getValue();
-                supplies.setLayoutStyle(layoutStyles.get(value));
+                final String value = (String)event.getValue();
+                suppliesItem.setLayoutStyle(LAYOUT_STYLES.get(value));
             }
-            
         });
 
-        form.setFields(supplies, spacer1, spacer2, layoutStyleSelector);
+        final DynamicForm configureForm = new DynamicForm();
+        configureForm.setIsGroup(true);
+        configureForm.setGroupTitle("Configure Multi ComboBox");
+        configureForm.setWidth100();
+        configureForm.setTitleOrientation(TitleOrientation.TOP);
+        configureForm.setItems(layoutStyleSelector);
 
-        return form;
+        final DynamicForm suppliesForm = new DynamicForm();
+        suppliesForm.setWidth100();
+        suppliesForm.setNumCols(1);
+        suppliesForm.setTitleOrientation(TitleOrientation.TOP);
+        suppliesForm.setItems(suppliesItem);
+
+        final VStack layout = new VStack(10);
+        layout.setWidth(500);
+        layout.setMembers(configureForm, suppliesForm);
+        return layout;
     }
 
     public String getIntro() {
