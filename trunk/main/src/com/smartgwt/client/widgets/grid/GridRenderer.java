993240ae7f9eaa49ec1732f1d8a32a24404d0d4d
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-09-10 08:06:04 +0000

    Add a few more functions to GridRenderer to support ID Management. [gm]

--- trunk/main/src/com/smartgwt/client/widgets/grid/GridRenderer.java
+++ trunk/main/src/com/smartgwt/client/widgets/grid/GridRenderer.java
@@ -1,15 +1,65 @@
+/*
+ * Smart GWT (GWT for SmartClient)
+ * Copyright 2008 and beyond, Isomorphic Software, Inc.
+ *
+ * Smart GWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  Smart GWT is also
+ * available under typical commercial license terms - see
+ * http://smartclient.com/license
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+/* sgwtgen */
+ 
 package com.smartgwt.client.widgets.grid;
 
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.core.client.JavaScriptObject;
-import com.smartgwt.client.widgets.Canvas;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.event.shared.*;
+import com.smartgwt.client.util.*;
+import com.smartgwt.client.util.workflow.*;
+import com.smartgwt.client.widgets.*;
 
 /**
- * The underlying grid canvas that offers consistent cross-platform sizing, clipping, and events.
+ * A flexible, high-speed table that offers consistent cross-platform sizing, clipping, and events.
  */
 public class GridRenderer extends Canvas {
 
-    public GridRenderer(JavaScriptObject jsObj) {
-        super(jsObj);
+    public native static GridRenderer getOrCreateRef(JavaScriptObject jsObj) /*-{
+        if (jsObj == null) return null;
+        var instance = jsObj["__ref"];
+        if (instance == null) {
+            instance = @com.smartgwt.client.widgets.grid.GridRenderer::new(Lcom/google/gwt/core/client/JavaScriptObject;)(jsObj);
+        }
+        return instance;
+    }-*/;
+
+    public void setJavaScriptObject(JavaScriptObject jsObj) {
+        internalSetID(jsObj);
+        JSOHelper.setObjectAttribute(jsObj, SC.REF, this);
+        onBind();
     }
-    
+
+    public GridRenderer(){
+        scClassName = "GridRenderer";
+    }
+
+    public GridRenderer(JavaScriptObject jsObj){
+        scClassName = "GridRenderer";
+        setJavaScriptObject(jsObj);
+        
+    }
+
+    protected native JavaScriptObject create()/*-{
+        var config = this.@com.smartgwt.client.widgets.BaseWidget::getConfig()();
+        var scClassName = this.@com.smartgwt.client.widgets.BaseWidget::scClassName;
+        var widget = $wnd.isc[scClassName].create(config);
+        this.@com.smartgwt.client.widgets.BaseWidget::doInit()();
+        return widget;
+    }-*/;
 }
