5b065ccc2769dfb7e6ee6aabe06bdafcc802fdc6
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-11-22 10:11:28 +0000

    - add a "website mode" and other enhancements to SC Feature Explorer and SGWT Showcase [eg]

--- samples/showcase/src/com/smartgwt/sample/showcase/client/Showcase.java
+++ samples/showcase/src/com/smartgwt/sample/showcase/client/Showcase.java
@@ -1,6 +1,7 @@
 package com.smartgwt.sample.showcase.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.HistoryListener;
@@ -8,24 +9,33 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.smartgwt.client.Version;
 import com.smartgwt.client.core.KeyIdentifier;
 import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.types.TabBarControls;
 import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.util.Page;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.ImgButton;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.Window;
 import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.DrawEvent;
+import com.smartgwt.client.widgets.events.DrawHandler;
+import com.smartgwt.client.widgets.events.ResizedEvent;
+import com.smartgwt.client.widgets.events.ResizedHandler;
+import com.smartgwt.client.widgets.events.VisibilityChangedEvent;
+import com.smartgwt.client.widgets.events.VisibilityChangedHandler;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangeEvent;
 import com.smartgwt.client.widgets.form.fields.events.ChangeHandler;
-import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.HStack;
 import com.smartgwt.client.widgets.layout.Layout;
 import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.layout.VStack;
 import com.smartgwt.client.widgets.menu.Menu;
 import com.smartgwt.client.widgets.menu.MenuItem;
 import com.smartgwt.client.widgets.menu.MenuItemIfFunction;
@@ -46,14 +56,30 @@ import com.smartgwt.sample.showcase.client.data.ExplorerTreeNode;
 
 import java.util.LinkedHashMap;
 
+@SuppressWarnings("deprecation")
 public class Showcase implements EntryPoint, HistoryListener {
+	
+SCConstants scConstants = (SCConstants)GWT.create(SCConstants.class);
+private boolean isc_websiteMode = scConstants.websiteMode();
 
     private TabSet mainTabSet;
     private SideNavTree sideNav;
     private Menu contextMenu;
+	private HLayout topPane;
+	private HLayout leftSuperPane;
+	private Canvas centerPane;
+	private VStack rightPane;
+	private HStack bottomPane;
+	private HLayout bottomPaneLeft;
+	private HLayout bottomPaneRight;
 
     public void onModuleLoad() {
         final String initToken = History.getToken();
+        // If the request contains param "websiteMode", override the configured value
+        String isc_websiteModeParam = com.google.gwt.user.client.Window.Location.getParameter("isc_websiteMode");
+        if(isc_websiteModeParam!=null) {
+        	isc_websiteMode = Boolean.parseBoolean(isc_websiteModeParam);
+        }
 
         //setup overall layout / viewport
         VLayout main = new VLayout() {
@@ -142,20 +168,26 @@ public class Showcase implements EntryPoint, HistoryListener {
 
         topBar.addSpacer(6);
 
-        main.addMember(topBar);
-
         main.setWidth100();
         main.setHeight100();
         main.setStyleName("tabSetContainer");
 
-        HLayout hLayout = new HLayout();
-        hLayout.setLayoutMargin(5);
-        hLayout.setWidth100();
-        hLayout.setHeight100();
-
+        VLayout featureExplorer = new VLayout();
+        featureExplorer.setLayoutMargin(5);
+        featureExplorer.setWidth100();
+        featureExplorer.setHeight100();
+        
+        topPane = new HLayout();
+        topPane.setHeight100();
+        topPane.setOverflow(Overflow.HIDDEN);
+        
+        leftSuperPane = new HLayout();
+        leftSuperPane.setResizeBarTarget("next");
+        leftSuperPane.setOverflow(Overflow.HIDDEN);
+        
         VLayout sideNavLayout = new VLayout();
         sideNavLayout.setHeight100();
-        sideNavLayout.setWidth(185);
+        sideNavLayout.setWidth(215);
         sideNavLayout.setShowResizeBar(true);
 
         sideNav = new SideNavTree();
@@ -182,8 +214,6 @@ public class Showcase implements EntryPoint, HistoryListener {
         toolStripVersion.addMember(version);
         sideNavLayout.addMember(toolStripVersion);
         
-        hLayout.addMember(sideNavLayout);
-
         mainTabSet = new TabSet();
 
         Layout paneContainerProperties = new Layout();
@@ -261,15 +291,164 @@ public class Showcase implements EntryPoint, HistoryListener {
 
         mainTabSet.addTab(tab);
 
-        Canvas canvas = new Canvas();
-        canvas.setBackgroundImage("[SKIN]/shared/background.gif");
-        canvas.setWidth100();
-        canvas.setHeight100();
-        canvas.addChild(mainTabSet);
+        centerPane = new Canvas();
+        centerPane.setBackgroundImage("[SKIN]/shared/background.gif");
+        centerPane.setWidth100();
+        centerPane.setHeight100();
+        centerPane.addChild(mainTabSet);
 
-        hLayout.addMember(canvas);
-        main.addMember(hLayout);
+        // Build and add the rightPane, with callToAction buttons
+        rightPane = new VStack();
+        rightPane.setDefaultLayoutAlign(Alignment.CENTER);
+        rightPane.setOverflow(Overflow.HIDDEN);
+        rightPane.setWidth(234);
+        rightPane.hide();
+                
+        // Add bottomPane
+        bottomPaneLeft = new HLayout();
+        bottomPaneLeft.setWidth("50%");
+        bottomPaneLeft.setHeight(30);
+        bottomPaneLeft.setAlign(Alignment.LEFT);
+        bottomPaneLeft.setLayoutLeftMargin(10);
+        bottomPaneLeft.setMembersMargin(10);
+        
+        bottomPaneRight = new HLayout();
+        bottomPaneRight.setWidth("50%");
+        bottomPaneRight.setHeight(30);
+        bottomPaneRight.setAlign(Alignment.RIGHT);
+        bottomPaneRight.setLayoutLeftMargin(10);
 
+        bottomPane = new HStack();
+        bottomPane.addMember(bottomPaneLeft);
+        bottomPane.addMember(bottomPaneRight);
+        bottomPane.setOverflow(Overflow.HIDDEN);
+        bottomPane.setHeight(30);
+        bottomPane.setWidth100();
+        bottomPane.setStyleName("explorerBottomPane");
+        bottomPane.hide();
+        
+        // Right Pane buttons
+        // SmartGWT Call to action button in right panel
+        Layout smartGwtButtonBoxR = new Layout();
+        smartGwtButtonBoxR.setStyleName("explorerButtonBoxR");
+        smartGwtButtonBoxR.setWidth(200);
+        rightPane.addMember(smartGwtButtonBoxR);
+        
+        Label smartGwtButtonR = new Label("<div style='font-size: 9pt;''>Prefer to write UI in Javascript?</div>" +
+					"<img src='images/icon_javascript_t.png'" +
+						"style='height: 30pt; float: left; margin-right: 2pt; " +
+					"margin-left: 2pt; margin-top: 10pt;'/>" +
+					"<a target='_top' style='line-height: 12pt; padding-top: 5pt;' "+
+					"href='" + "http://www.smartclient.com/#Welcome" + "' " +
+					">" + "Smart Client<br/>Hands-On Demo" + "</a>");
+        smartGwtButtonR.setStyleName("darkgrey_Ebutton ERbutton");
+        smartGwtButtonR.setWidth(200);
+        smartGwtButtonR.setHeight(1);
+        smartGwtButtonR.setAlign(Alignment.CENTER);
+        smartGwtButtonR.setValign(VerticalAlignment.CENTER);
+        smartGwtButtonBoxR.addMember(smartGwtButtonR);
+        
+        // Contact Us Call to action button in right panel
+        Layout contactUsButtonBoxR = new Layout();
+        contactUsButtonBoxR.setStyleName("explorerButtonBoxR explorerButtonBoxGradient");
+        contactUsButtonBoxR.setWidth(200);
+        rightPane.addMember(contactUsButtonBoxR);
+        
+        Label contactUsButtonR = new Label("<div>Got questions?" +
+        		  "<div style='font-size: 8pt;'>We'd love to hear from you!</div>" +
+        		  "</div>" +
+					"<a target='_top' href='" + "/company/contact.jsp" + "' " +
+					">" + "Contact Us" + "</a>");
+        contactUsButtonR.setStyleName("darkgrey_Ebutton ERbutton");
+        contactUsButtonR.setWidth(200);
+        contactUsButtonR.setHeight(1);
+        contactUsButtonR.setAlign(Alignment.CENTER);
+        contactUsButtonR.setValign(VerticalAlignment.CENTER);
+        contactUsButtonBoxR.addMember(contactUsButtonR);
+
+        // Free Trial Call to action button in right panel
+        Layout freeTrialButtonBoxR = new Layout();
+        freeTrialButtonBoxR.setStyleName("explorerButtonBoxR explorerButtonBoxGradient");
+        freeTrialButtonBoxR.setWidth(200);
+        rightPane.addMember(freeTrialButtonBoxR);
+        
+        Label freeTrialButtonR = new Label("<div>Try it out for yourself!</div>" +
+        			"<a target='_top' style='line-height: 12pt; padding-top: 5pt;' " +
+        			"href='" + "/product/download.jsp" + "' " +
+        			">" + "Free Trial<br/><span style='font-size: 8pt;'>60 days</span>" + "</a>");
+        freeTrialButtonR.setStyleName("blue_Ebutton ERbutton");
+        freeTrialButtonR.setWidth(200);
+        freeTrialButtonR.setHeight(1);
+        freeTrialButtonR.setAlign(Alignment.CENTER);
+        freeTrialButtonR.setValign(VerticalAlignment.CENTER);
+        freeTrialButtonBoxR.addMember(freeTrialButtonR);
+
+        // Pricing Call to action button in right panel
+        Layout pricingTrialButtonBoxR = new Layout();
+        pricingTrialButtonBoxR.setStyleName("explorerButtonBoxR explorerButtonBoxGradient");
+        pricingTrialButtonBoxR.setWidth(200);
+        rightPane.addMember(pricingTrialButtonBoxR);
+        
+        Label pricingTrialButtonR = new Label("<div>Want your own?</div>" +
+				"<a target='_top' href='" + "/product/" + "' " +
+				">" + "Editions & Pricing" + "</a>");
+        pricingTrialButtonR.setStyleName("orange_Ebutton ERbutton");
+        pricingTrialButtonR.setWidth(200);
+        pricingTrialButtonR.setHeight(1);
+        pricingTrialButtonR.setAlign(Alignment.CENTER);
+        pricingTrialButtonR.setValign(VerticalAlignment.CENTER);
+        pricingTrialButtonBoxR.addMember(pricingTrialButtonR);
+
+        // Learn More Call to action button in right panel
+        Layout learnMoreButtonBoxR = new Layout();
+        learnMoreButtonBoxR.setStyleName("explorerButtonBoxR explorerButtonBoxGradient");
+        learnMoreButtonBoxR.setWidth(200);
+        rightPane.addMember(learnMoreButtonBoxR);
+        
+        Label learnMoreButtonR = new Label("<div>Isomorphic has the advantage.</div>" +
+				"<a target='_top' href='" + "technology/whysmart.jsp" + "' " +
+				">" + "Learn More >" + "</a>");
+        learnMoreButtonR.setStyleName("darkgrey_Ebutton ERbutton");
+        learnMoreButtonR.setWidth(200);
+        learnMoreButtonR.setHeight(1);
+        learnMoreButtonR.setAlign(Alignment.CENTER);
+        learnMoreButtonR.setValign(VerticalAlignment.CENTER);
+        learnMoreButtonBoxR.addMember(learnMoreButtonR);
+
+        // Bottom Pane buttons
+        // Free Trial Call to action button in bottom panel
+        Label freeTrialButtonB = new Label("<a target='_top' href='/product/download.jsp' " +
+    			">Free Trial</a>");
+        freeTrialButtonB.setStyleName("blue_Ebutton EBbutton");
+        bottomPaneLeft.addMember(freeTrialButtonB);
+        
+        // Pricing Call to action button in bottom panel
+        Label pricingButtonB = new Label("<a target='_top' href='/product/' " +
+			">Editions & Pricing</a>");
+        pricingButtonB.setStyleName("orange_Ebutton EBbutton");
+        bottomPaneLeft.addMember(pricingButtonB);
+
+        // SmartGWT Call to action button in bottom panel
+        Label smartGWTButtonPreB = new Label("<span>Prefer to write UI in Java?</span>");
+        smartGWTButtonPreB.setStyleName("EBbutton");
+        smartGWTButtonPreB.setWidth("180pt");
+        bottomPaneRight.addMember(smartGWTButtonPreB);
+        Label smartGWTButtonB = new Label("<a target='_top' style='width: 130pt;' " +
+			"href='http://www.smartclient.com/smartgwtee/showcase'" +
+			" >Smart GWT Live Demo</a>");
+        smartGWTButtonB.setStyleName("darkgrey_Ebutton EBbutton");
+        bottomPaneRight.addMember(smartGWTButtonB);
+
+        leftSuperPane.addMember(sideNavLayout);
+        leftSuperPane.addMember(centerPane);
+        topPane.addMember(leftSuperPane);
+        topPane.addMember(rightPane);
+        featureExplorer.addMember(topPane);
+        featureExplorer.addMember(bottomPane);
+
+        main.addMember(topBar);
+        main.addMember(featureExplorer);
+        
         if (SC.hasFirebug()) {
             Label label = new Label();
             label.setWidth100();
@@ -286,16 +465,84 @@ public class Showcase implements EntryPoint, HistoryListener {
             fbWindow.setRedrawOnResize(true);
             main.addMember(fbWindow);
         }
+        
+        main.addDrawHandler(new DrawHandler() {
+			@Override
+			public void onDraw(DrawEvent event) {
+				pageRedrawn();
+			}
+		});
+                
+        main.addResizedHandler(new ResizedHandler() {
+			@Override
+			public void onResized(ResizedEvent event) {
+				pageResized();
+			}
+		});
+        
+        rightPane.addVisibilityChangedHandler(new VisibilityChangedHandler() {
+			@Override
+			public void onVisibilityChanged(VisibilityChangedEvent event) {
+				rightPaneVisibilityChanged(event.getIsVisible());
+			}
+		});
 
         main.draw();
-
+        
         // Add history listener
         History.addHistoryListener(this);
 
         RootPanel p = RootPanel.get("loadingWrapper");
-        if (p != null) RootPanel.getBodyElement().removeChild(p.getElement());
+        if (p != null) RootPanel.getBodyElement().removeChild(p.getElement());        
     }
+    
+	private void pageRedrawn() {
+    	if(isc_websiteMode ==true) {
+			leftSuperPane.setShowResizeBar(true);
+			// If browser width > 900px, show right, else show bottom
+			if(Page.getWidth()>900) {
+				rightPane.show();
+				bottomPane.hide();
+			} else {
+				bottomPane.show();
+				rightPane.hide();
+			}
+		}
+	}
 
+    private void rightPaneVisibilityChanged(boolean isVisible) {
+    	if(isc_websiteMode ==true) {
+    		if(Page.getWidth()<=900) {
+    			rightPane.hide();
+    			bottomPane.show();
+    			return;
+    		}
+    		if(isVisible) {
+    			centerPane.setHeight100();
+    			bottomPane.hide();
+    			rightPane.show();
+    		} else {
+    			centerPane.setHeight(leftSuperPane.getHeight()-bottomPane.getHeight());
+    			bottomPane.show();
+    			rightPane.hide();
+    		}
+    	}
+    }
+    
+    private void pageResized() {
+    	if(isc_websiteMode ==true) {
+    		if(leftSuperPane!=null && Page.getWidth()<=900) {
+    			if(leftSuperPane.getShowResizeBar()) {
+    				rightPane.hide();
+    				bottomPane.show();
+    			} else {
+    				rightPane.show();
+    				bottomPane.hide();    				
+    			}
+    		}
+    	}
+    }
+       
     private Menu createContextMenu() {
         Menu menu = new Menu();
         menu.setWidth(140);
