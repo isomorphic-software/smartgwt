26668f12d14ec5aba9d44feaccf983a68df93250
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2013-11-27 02:08:30 +0000

    FacetChart DataLabelHover and LegendHover customizers added. [at]

---
+++ trunk/main/src/com/smartgwt/client/widgets/chart/LegendHoverCustomizer.java
@@ -0,0 +1,8 @@
+package com.smartgwt.client.widgets.chart;
+
+import com.smartgwt.client.widgets.cube.FacetValue;
+
+public interface LegendHoverCustomizer {
+	
+	 String hoverHTML(FacetValue facetValue,FacetValue metricFacetValue);
+}
