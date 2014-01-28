7c983e804b4b0672819fb09af0deae1f608fcf9b
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-01-28 02:46:28 +0000

    Removed the custom calendar-attribute code in a bunch of customizers - its available in all cases via getCalendar() on the passed CalendarView

--- branches/5.0/main/src/com/smartgwt/client/widgets/calendar/DisableDateCustomizer.java
+++ branches/5.0/main/src/com/smartgwt/client/widgets/calendar/DisableDateCustomizer.java
@@ -6,23 +6,10 @@ import java.util.Date;
  * Called to determine whether the passed date should be considered disabled in the passed view
  */
 public abstract class DisableDateCustomizer {
-    
-    private Calendar calendar;
-
-    public void setCalendar(Calendar calendar) {
-        this.calendar = calendar;
-    }
-    /**
-     * Returns the Calendar in which the cell for the passed date exists
-     * @return Calendar in which the date is being rendered
-     */
-    public Calendar getCalendar() {
-        return this.calendar;
-    }
-    
 
     /**
      * disableDate() should be implemented and return true if the passed date should be considered disabled in the passed view
+     * To access the Calendar itself, use calendarView.getCalendar().
      * @param date the date in question
      * @param calendarView the view in which the date cell exists
      * @return true if the date should be considered disabled, or false otherwise
--- branches/5.0/main/src/com/smartgwt/client/widgets/calendar/EventStyleCustomizer.java
+++ branches/5.0/main/src/com/smartgwt/client/widgets/calendar/EventStyleCustomizer.java
@@ -6,23 +6,9 @@ import java.util.Date;
  * Called to determine the CSS class name for a given event in a given view
  */
 public abstract class EventStyleCustomizer {
-    
-    private Calendar calendar;
-
-    public void setCalendar(Calendar calendar) {
-        this.calendar = calendar;
-    }    
-    /**
-     * Returns the Calendar in which the event is being rendered
-     * @return Calendar in which the event is being rendered
-     */
-    public Calendar getCalendar() {
-        return this.calendar;
-    }
-    
-
     /**
      * getEventStyleStyle() should be implemented and return the CSS class name for the passed event in the passed view
+     * To access the Calendar itself, use calendarView.getCalendar().
      * @param calendarEvent the event being rendered
      * @param calendarView the view in which the event is being rendered
      * @return CSS class to apply to the EventCanvas for the passed event in the passed view
