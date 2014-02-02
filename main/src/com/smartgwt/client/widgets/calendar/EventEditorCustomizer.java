9108b551f5385a4183df4fadf830fc7e98797960
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-02-02 09:43:26 +0000

    New Calendar customizers, EventDialogCustomizer and EventEditorCustomizer

---
+++ main/src/com/smartgwt/client/widgets/calendar/EventEditorCustomizer.java
@@ -0,0 +1,30 @@
+package com.smartgwt.client.widgets.calendar;
+
+import java.util.Date;
+
+/**
+ * Called whenever a Calender needs to show an EventEditor, to edit or create a CalendarEvent
+ */
+public abstract class EventEditorCustomizer {
+    private Calendar calendar;
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+    /**
+     * Returns the Calendar in which the date is being rendered
+     * @return Calendar in which the date is being rendered
+     */
+    public Calendar getCalendar() {
+        return this.calendar;
+    }
+
+    /**
+     * showEventEditor() can be used to show some custom user interface for editing or creating calendar events.
+     * To access the Calendar itself, use calendarView.getCalendar().
+     * @param calendarEvent the event being rendered
+     * @param calendarView the view in which the event is being rendered
+     * @return false to prevent the default Event Editor from showing
+     */
+    public abstract Boolean showEventEditor(CalendarEvent calendarEvent, Boolean isNewEvent);
+}
