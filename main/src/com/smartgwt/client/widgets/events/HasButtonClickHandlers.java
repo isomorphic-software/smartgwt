038589525a079370e1c0c785558576ab18d593e6
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-04-23 17:21:25 +0000

    - check on probable bug: if a non-button is added to a Dialog's toolbar via the SGWT   setToolbarButtons() API, it looks like buttonClick would fire (see implementation in   Dialog.js near "toolbarDefaults").  If so, fix so that buttonClick only fires if the clicked   member is a subclass of StatefulCanvas.   - fix in mainline, 4.1, 4.0 and 3.1 [eg]

--- main/src/com/smartgwt/client/widgets/events/HasButtonClickHandlers.java
+++ main/src/com/smartgwt/client/widgets/events/HasButtonClickHandlers.java
@@ -13,13 +13,13 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  */
+/* sgwtgen */
  
 package com.smartgwt.client.widgets.events;
 
 import com.smartgwt.client.event.*;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.HasHandlers;
-
 public interface HasButtonClickHandlers extends HasHandlers {
     /**
      * Fires when any button in this Dialog's toolbar is clicked.  Default implementation does nothing.
@@ -29,3 +29,4 @@ public interface HasButtonClickHandlers extends HasHandlers {
      */
     HandlerRegistration addButtonClickHandler(ButtonClickHandler handler);
 }
+
