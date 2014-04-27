22dbcfbc0ebc7d3c6eabf997b45cfb019b6874c9
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-04-27 18:32:45 +0000

    As these three classes are not generated anymore, the "/* sgwtgen */" comment must be removed. [eg]

--- main/src/com/smartgwt/client/widgets/events/HasButtonClickHandlers.java
+++ main/src/com/smartgwt/client/widgets/events/HasButtonClickHandlers.java
@@ -13,7 +13,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  */
-/* sgwtgen */
  
 package com.smartgwt.client.widgets.events;
 
