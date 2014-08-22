2aeaf4c6e5ff33f4bc0d04b9c00eccfc60d46de6
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-08-22 13:33:51 +0000

    Added four new Timeline samples to the LGPL Showcase - Filtering, Grouping, Resolution and Zones & Indicators Tweaked TimelineData and added TimelineLaneData

--- samples/showcase/src/com/smartgwt/sample/showcase/client/calendar/TimelineZonesAndIndicatorsSample.java
+++ samples/showcase/src/com/smartgwt/sample/showcase/client/calendar/TimelineZonesAndIndicatorsSample.java
@@ -3,22 +3,23 @@ package com.smartgwt.sample.showcase.client.calendar;
 import java.util.Date;
 
 import com.smartgwt.client.types.TimeUnit;
-import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.calendar.CalendarEvent;
 import com.smartgwt.client.widgets.calendar.HeaderLevel;
 import com.smartgwt.client.widgets.calendar.Timeline;
 import com.smartgwt.client.widgets.grid.ListGridField;
+
+import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.sample.showcase.client.PanelFactory;
 import com.smartgwt.sample.showcase.client.ShowcasePanel;
 import com.smartgwt.sample.showcase.client.data.TimelineData;
 import com.smartgwt.sample.showcase.client.data.TimelineLaneData;
 
-public class SimpleTimelineSample extends ShowcasePanel {
+public class TimelineZonesAndIndicatorsSample extends ShowcasePanel {
 
-    private static final String DESCRIPTION = "<p>This example uses a Timeline, a simple subclass of Calendar, to show a variety of " +
-            "development tasks assigned to developers over a 3-week period.  This timeline is bound " +
-            "to an array of event data. Drag events to alter their start dates, and resize events to " +
-            "alter their durations. Hover over an event to see it's details or click to edit them.</p>";
+    private static final String DESCRIPTION = "<p>This example shows Calendar Zones and " +
+            "Indicators.  Zones are semi-transparent styled areas that sit behind event " +
+            "canvases and span all lanes.  Indicators are vertical lines that represents a " +
+            "specific moment in time and also span all lanes.</p>";
 
 
     public static class Factory implements PanelFactory {
@@ -26,7 +27,7 @@ public class SimpleTimelineSample extends ShowcasePanel {
         private String id;
 
         public ShowcasePanel create() {
-            SimpleTimelineSample panel = new SimpleTimelineSample();
+            TimelineZonesAndIndicatorsSample panel = new TimelineZonesAndIndicatorsSample();
             id = panel.getID();
             return panel;
         }
@@ -41,25 +42,39 @@ public class SimpleTimelineSample extends ShowcasePanel {
     }
 
     public Canvas getViewPanel() {
+        HeaderLevel[] headerLevels = new HeaderLevel[]{
+            new HeaderLevel(TimeUnit.WEEK), 
+            new HeaderLevel(TimeUnit.DAY)
+        };
+        
         Timeline calendar = new Timeline();
         calendar.setHeight(451);
         calendar.setStartDate(new Date(112, 5, 2));
         calendar.setEndDate(new Date(112, 5, 22));
         calendar.setCanEditLane(true);
         calendar.setShowEventDescriptions(false);
-
-        HeaderLevel[] headerLevels = new HeaderLevel[]{
-            new HeaderLevel(TimeUnit.WEEK), 
-            new HeaderLevel(TimeUnit.DAY)
-        };
+        calendar.setEventSnapGap(1); // snap to 1 minute intervals
+        calendar.setLaneEventPadding(2); // add a little space around events
+        // zone settings
+        calendar.setShowZones(true);
+        calendar.setZones(
+        	new CalendarEvent(-1, "June 3-6", "", new Date(112, 5, 3), new Date(112, 5, 6)),
+        	new CalendarEvent(-2, "June 10-14", "", new Date(112, 5, 10), new Date(112, 5, 14))
+        );
+        // indicator settings
+        calendar.setShowIndicators(true);
+        calendar.setIndicators(
+        	new CalendarEvent(-3, "June 15 - noon", "Team Lunch", new Date(112, 5, 15, 12, 0), null)
+        );
+        // set up the grid 
         calendar.setHeaderLevels(headerLevels);
-        calendar.setLaneFields(new ListGridField[]{ new ListGridField("title", "Developer", 120)});
         calendar.setLanes(TimelineLaneData.getRecords());
+        calendar.setLaneFields(new ListGridField[]{ new ListGridField("title", "Developer", 120)});
         calendar.setData(TimelineData.getRecords());
 
         return calendar;
     }
-
+    
     public String getIntro() {
         return DESCRIPTION;
     }
