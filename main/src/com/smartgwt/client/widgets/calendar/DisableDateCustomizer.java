4fbca496386c6cd9a8cba8a71b193a6a66c84232
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-08-26 00:33:35 +0000

    Fixed and enhanced a few Timeline Customizers with a cancel() mechanism - effectively cancels the customizer, and executes the default method implementation instead.

--- main/src/com/smartgwt/client/widgets/calendar/DisableDateCustomizer.java
+++ main/src/com/smartgwt/client/widgets/calendar/DisableDateCustomizer.java
@@ -2,17 +2,32 @@ package com.smartgwt.client.widgets.calendar;
 
 import java.util.Date;
 
+import com.smartgwt.client.widgets.calendar.*;
+
 /**
  * Called to determine whether the passed date should be considered disabled in the passed view
  */
 public abstract class DisableDateCustomizer {
+    /**
+     * Call cancel() to prevent the customization, and have the customizer return the result of the original method being customized
+     */
+    public boolean cancelled = false;
+    public void cancel () {
+        this.cancelled = true;
+    }
+    public boolean isCancelled () {
+        return this.cancelled;
+    }
+    protected void setCancelled (boolean cancel) {
+        this.cancelled = cancel;
+    }
 
     /**
      * disableDate() should be implemented and return true if the passed date should be considered disabled in the passed view
      * To access the Calendar itself, use calendarView.getCalendar().
      * @param date the date in question
      * @param calendarView the view in which the date cell exists
-     * @return true if the date should be considered disabled, or false otherwise
+     * @return true if the date should be considered disabled, or false otherwise - return null to fall back on default behavior
      */
-    public abstract boolean disableDate(Date date, CalendarView calendarView);
+    public abstract boolean shouldDisableDate(Date date, CalendarView calendarView);
 }
