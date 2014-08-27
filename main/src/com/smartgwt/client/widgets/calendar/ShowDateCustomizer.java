0102b5fc161a5c06e101e47356cdda54d47509ad
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2014-08-27 16:01:50 +0000

    Updated various Calendar-related customizers to include a cancel mechanism

--- main/src/com/smartgwt/client/widgets/calendar/ShowDateCustomizer.java
+++ main/src/com/smartgwt/client/widgets/calendar/ShowDateCustomizer.java
@@ -2,16 +2,32 @@ package com.smartgwt.client.widgets.calendar;
 
 import java.util.Date;
 
+import com.smartgwt.client.widgets.calendar.*;
+
 /**
- * Called to determine a particular date should be visible in the passed view
+ * Called to determine whether a particular date should be visible in the passed view
  */
 public abstract class ShowDateCustomizer {
+    /**
+     * Call cancel() to prevent the customization, and have the customizer return the result of the original method being customized
+     */
+    public boolean cancelled = false;
+    public void cancel () {
+        this.cancelled = true;
+    }
+    public boolean isCancelled () {
+        return this.cancelled;
+    }
+    protected void setCancelled (boolean cancel) {
+        this.cancelled = cancel;
+    }
+
     /**
      * showDate() should be implemented and return true if the passed date should be visible in the passed view
      * To access the Calendar itself, use calendarView.getCalendar().
      * @param date the date in question
      * @param calendarView the view in which the date cell exists
-     * @return true if the date should be visible in the passed view, or false otherwise
+     * @return true if the date should be visible in the passed view, or false otherwise - return null to fall back on default behavior
      */
-    public abstract boolean showDate(Date date, CalendarView calendarView);
+    public abstract boolean shouldShowDate(Date date, CalendarView calendarView);
 }
--- main/src/com/smartgwt/client/widgets/calendar/ShowLaneCustomizer.java
+++ main/src/com/smartgwt/client/widgets/calendar/ShowLaneCustomizer.java
@@ -2,16 +2,32 @@ package com.smartgwt.client.widgets.calendar;
 
 import java.util.Date;
 
+import com.smartgwt.client.widgets.calendar.*;
+
 /**
  * Called to determine a particular lane should be visible in the passed view
  */
 public abstract class ShowLaneCustomizer {
     /**
+     * Call cancel() to prevent the customization, and have the customizer return the result of the original method being customized
+     */
+    public boolean cancelled = false;
+    public void cancel () {
+        this.cancelled = true;
+    }
+    public boolean isCancelled () {
+        return this.cancelled;
+    }
+    protected void setCancelled (boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    /**
      * showLane() should be implemented and return true if the passed lane should be visible in the passed view
      * To access the Calendar itself, use calendarView.getCalendar().
      * @param lane the lane in question
      * @param calendarView the view in which the lane exists
-     * @return true if the lane should be visible in the passed view, or false otherwise
+     * @return true if the lane should be visible in the passed view, or false otherwise - return null to fall back on default behavior
      */
-    public abstract boolean showLane(Lane lane, CalendarView calendarView);
+    public abstract boolean shouldShowLane(Lane lane, CalendarView calendarView);
 }
