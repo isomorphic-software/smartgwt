6c966628eeed64459ee3635dc69637e299f4b86c
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2015-01-25 06:36:22 +0000

    - Add configuration API ConfigUtil.setUseWarningsForConfiguraitonErrors() to control   whether an IllegalStateException is thrown instead of logging for certain errors. - Add additional Java argument signature for ConfigUtil.warnofPreConfigInstantiation(). [gm]

--- trunk/main/src/com/smartgwt/client/util/ConfigUtil.java
+++ trunk/main/src/com/smartgwt/client/util/ConfigUtil.java
@@ -27,17 +27,28 @@ public class ConfigUtil {
         return className.replaceFirst(".*\\.", "");
     }
 
-    // BaseWidget/BaseClass
+    // BaseWidget/BaseClass/FormItem
+
+    public static void warnOfPreConfigInstantiation(Class callerClass, String callerMethodName,
+                                                    Class configClass)
+    {
+        warnOfPreConfigInstantiation(callerClass, callerMethodName, 
+                                     getSimpleClassName(configClass));
+    }
 
     public static void warnOfPreConfigInstantiation(Class callerClass, String callerMethodName, 
                                                     String configTypeName) 
     {
         String className = getSimpleClassName(callerClass),
                callerDesc = className + "." + callerMethodName + "()";
-        SC.logWarn(callerDesc + ": the " + configTypeName + " passed as configuration " + 
+
+        String message = callerDesc + ": the " + configTypeName + " passed as configuration " +
             "properties has been initialized and/or drawn.  Calling draw(), fetchData() " +
             "and other key methods will force a component to initialize after which it " +
-            "cannot be used as configuration properties.", CONFIG_PROPERTIES);
+            "cannot be used as configuration properties.";
+
+        if (useWarnings) SC.logWarn(message, CONFIG_PROPERTIES);
+        else throw new IllegalStateException(message);
     }
 
     public static void warnOfPostConfigInstantiation(Class callerClass, String callerMethodName, 
@@ -91,5 +102,18 @@ public class ConfigUtil {
                callerDesc = className + "." + callerMethodName + "()";
             SC.logDebug(callerDesc + ": the " + configTypeName + " passed will be applied to " +
                         "this " + className + " instance", CONFIG_PROPERTIES);
+    }
+
+    private static boolean useWarnings;
+
+    /**
+     * When set true, prevents IllegalStateException from being thrown when a configuration
+     * properties object is instantiated prior to being passed in to configure another object.
+     * A warning will be logged in this case instead of throwing an exception.
+     *
+     * @param useWarnings whether to report all config errors as warnings rather than exceptions
+     */
+    public static void setUseWarningsForConfigurationErrors(boolean useWarnings) {
+        ConfigUtil.useWarnings = useWarnings;
     }
 }
