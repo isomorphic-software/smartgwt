637a772be66f2ac5781d02894a9ab3cb6dc99b3c
smartgwt.dev <smartgwt.dev@97ff703d-2b4d-0410-aac2-33a30ea07567>
2015-02-16 14:34:02 +0000

    Merged r3078 from trunk.
    [dt]

--- branches/4.1/main/src/com/smartgwt/client/util/JSOHelper.java
+++ branches/4.1/main/src/com/smartgwt/client/util/JSOHelper.java
@@ -1355,6 +1355,7 @@ public class JSOHelper {
     /*-{
         if (properties != null) {
             if (copyProperties) properties = $wnd.isc.addProperties({}, properties);
+            delete properties.AUTOIDClass;
             delete properties.ID;
             delete properties.__ref;
             delete properties.__module;
